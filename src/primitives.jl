f3b892f05d546854c7bdc8a48d840ce483354c14
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-05 13:12:08 -0700

    basemeasures.jl => primitives.jl

---
+++ src/primitives.jl
@@ -0,0 +1,41 @@
+export isprimitive
+
+"""
+    isprimitive(μ)
+
+Most measures are defined in terms of other measures, for example using a
+density or a pushforward. Those that are not are considered (in this library,
+it's not a general measure theory thing) to be _primitive_. The canonical
+example of a primitive measure is `Lebesgue(X)` for some `X`.
+
+The default method is
+    isprimitive(μ) = false
+
+So when adding a new primitive measure, it's necessary to add a method for its type
+that returns `true`.
+"""
+isprimitive(μ) = false
+isprimitive(::PrimitiveMeasure) = true
+
+abstract type PrimitiveMeasure <: AbstractMeasure end
+
+
+
+export basemeasure
+
+"""
+    basemeasure(μ)
+
+Many measures are defined in terms of a logdensity relative to some base
+measure. This makes it important to be able to find that base measure.
+
+For measures not defined in this way, we'll typically have `basemeasure(μ) == μ`.
+"""
+function basemeasure end
+
+basemeasure(μ::PrimitiveMeasure) = μ
+
+include("primitives/trivial.jl")
+include("primitives/lebesgue.jl")
+include("primitives/counting.jl")
+include("primitives/dirac.jl")
