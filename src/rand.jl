e34c6b132630efb95f8d789706be1fb5f66c145e
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-16 16:42:47 -0700

    Move `rand` method to MeasureTheory (references Distributions)

--- src/rand.jl
+++ src/rand.jl
@@ -6,8 +6,6 @@ Base.rand(T::Type, μ::AbstractMeasure) = rand(Random.GLOBAL_RNG, T, μ)
 
 Base.rand(rng::AbstractRNG, d::AbstractMeasure) = rand(rng, Float64, d)
 
-Base.rand(rng::AbstractRNG, T::Type, d::ParameterizedMeasure) = rand(rng, distproxy(d))
-
 @inline Random.rand!(d::AbstractMeasure, arr::AbstractArray) = rand!(GLOBAL_RNG, d, arr)
 
 
