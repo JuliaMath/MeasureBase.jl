602d043a22b6d7abc180ededfd26012758159b3b
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-22 10:10:08 -0700

    params

--- src/parameterized.jl
+++ src/parameterized.jl
@@ -38,11 +38,13 @@ end
 
 export params
 
-params(μ::ParameterizedMeasure) = getfield(μ, :par)
+params(::Type{PM}) where {N, PM<:ParameterizedMeasure{N}} = N
 
-function params(::Type{M}, constraints::NamedTuple{N2}=NamedTuple()) where {N1, N2, M<: ParameterizedMeasure{N1}} 
-thekeys = tuple((k for k in N1 if k ∉ N2)...)
+function params(::Type{M}, constraints::NamedTuple{N2}) where {N1, N2, M<: ParameterizedMeasure{N1}} 
+    tuple((k for k in N1 if k ∉ N2)...)
 end
+
+params(::Type{A}, nt::NamedTuple{C}) where {A<:Affine{N,M}} where {N,M,C} = tuple(setdiff(union(N, params(M)),C)...)
 
 params(μ) = ()
 
