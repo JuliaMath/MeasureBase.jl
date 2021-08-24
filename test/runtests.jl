5493946a4740c76759235d6ee01fb87ec44d3661
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-24 13:39:18 -0700

    drop commented-out tests for code not in MeasureBase

--- test/runtests.jl
+++ test/runtests.jl
@@ -151,20 +151,6 @@ end
 # (a::AffineMap)(x) = a.B*x + a.β
 # (a::AffineMap)(p::Normal) = Normal(μ = a.B*mean(p) + a.β, σ = sqrt(a.B*p.σ^2*a.B'))
 
-# @testset "DynamicFor" begin
-#     mc = Chain(Normal(μ=0.0)) do x Normal(μ=x) end
-#     r = rand(mc)
-   
-#     # Check that `r` is now deterministic
-#     @test logdensity(mc, take(r, 100)) == logdensity(mc, take(r, 100))
-    
-#     d2 = For(r) do x Normal(μ=x) end  
-
-#     @test_broken let r2 = rand(d2)
-#         logdensity(d2, take(r2, 100)) == logdensity(d2, take(r2, 100))
-#     end
-# end
-
 
 # @testset "Likelihood" begin
 #     dps = [
