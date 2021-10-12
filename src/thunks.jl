b458e6c4977d0720120ce5a2e16bb40f07dd45d5
Chad Scherrer <chad.scherrer@gmail.com>
2021-10-12 13:05:21 -0700

    comment out WIP code for now

--- src/thunks.jl
+++ src/thunks.jl
@@ -1,25 +1,25 @@
-abstract type LogdensityComputation end
+# abstract type LogdensityComputation end
 
-struct LogpdfComputation end
+# struct LogpdfComputation end
 
-struct BasemeasureComputation{B} <: LogdensityComputation
-    base::B
-end
+# struct BasemeasureComputation{B} <: LogdensityComputation
+#     base::B
+# end
 
-struct LogdensityThunk{C,M,X,L}
-    computation::C
-    measure::M
-    x::X
-    ℓ::L
-end
+# struct LogdensityThunk{C,M,X,L}
+#     computation::C
+#     measure::M
+#     x::X
+#     ℓ::L
+# end
 
-function reduce_step(thunk :: LogdensityThunk, callback=Returns(nothing))
+# function reduce_step(thunk :: LogdensityThunk, callback=Returns(nothing))
     
 
 
-reduce_step(LogdensityThunk{M,B,X,L}, callback=Returns(nothing)) -> Union{LogdensityThunk, L}
+# reduce_step(LogdensityThunk{M,B,X,L}, callback=Returns(nothing)) -> Union{LogdensityThunk, L}
 
-function logdensity(μ, b, x; cb=Returns(nothing))
-    f(thunk) = reduce_step(thunk, cb)
-    fix(f, LogdensityThunk(μ, b, x, zero(Float64)))
-end
+# function logdensity(μ, b, x; cb=Returns(nothing))
+#     f(thunk) = reduce_step(thunk, cb)
+#     fix(f, LogdensityThunk(μ, b, x, zero(Float64)))
+# end
