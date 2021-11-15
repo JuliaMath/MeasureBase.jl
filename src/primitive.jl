148412ab5f6b5068d8847779892ba60e59abce98
Chad Scherrer <chad.scherrer@gmail.com>
2021-11-15 10:55:17 -0800

    primitive measure docs

--- src/primitive.jl
+++ src/primitive.jl
@@ -1,5 +1,18 @@
 export PrimitiveMeasure
 
+"""
+    abstract type PrimitiveMeasure <: AbstractMeasure end    
+
+In the MeasureTheory ecosystem, a _primitive_ measure is a measure for which the
+definition and construction do not depend on any other measure. Primitive
+measures satisfy the following laws:
+
+    basemeasure(μ::PrimitiveMeasure) = μ
+
+    logdensity(μ::PrimitiveMeasure, x) = 0.0
+
+    logdensity(μ::M, ν::M, x) where {M<:PrimitiveMeasure} = 0.0
+"""
 abstract type PrimitiveMeasure <: AbstractMeasure end
 
 basemeasure(μ::PrimitiveMeasure) = μ
