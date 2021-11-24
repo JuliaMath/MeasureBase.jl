7207b35f886991b208fca5dcefd6ff06617f41b3
Chad Scherrer <chad.scherrer@gmail.com>
2021-11-24 14:29:47 -0800

    more domain mucking

--- src/domains.jl
+++ src/domains.jl
@@ -2,24 +2,37 @@ export IntegerRange
 
 abstract type AbstractDomain end
 
-struct RealBounds{L,U} <: AbstractDomain
+abstract type RealDomain <: AbstractDomain end
+
+struct RealNumbers <: RealDomain end
+
+const ℝ = RealNumbers()
+
+Base.minimum(::RealNumbers) = static(-Inf)
+Base.maximum(::RealNumbers) = static(Inf)
+
+Base.in(x, ::RealNumbers) = isreal(x)
+
+Base.show(io::IO, ::typeof(ℝ)) = print(io, "ℝ")
+
+struct BoundedReals{L,U} <: RealDomain
     lower :: L
     upper :: U
 end
 
-Base.in(x, b::RealBounds) = b.lower ≤ x ≤ b.upper
+Base.in(x, b::BoundedReals) = b.lower ≤ x ≤ b.upper
 
 
-const ℝ = RealBounds(static(-Inf), static(Inf))
+export ℝ, ℝ₊, 𝕀
 
-const ℝ₊ = RealBounds(static(0.0), static(Inf))
 
-const 𝕀 = RealBounds(static(0.0), static(1.0))
+const ℝ₊ = BoundedReals(static(0.0), static(Inf))
+const 𝕀 = BoundedReals(static(0.0), static(1.0))
 
-Base.minimum(b::RealBounds) = b.lower
-Base.maximum(b::RealBounds) = b.upper
+Base.minimum(b::BoundedReals) = b.lower
+Base.maximum(b::BoundedReals) = b.upper
 
-Base.show(io::IO, ::typeof(ℝ)) = print(io, "ℝ")
+
 Base.show(io::IO, ::typeof(ℝ₊)) = print(io, "ℝ₊")
 Base.show(io::IO, ::typeof(𝕀)) = print(io, "𝕀")
 
@@ -27,31 +40,37 @@ testvalue(::typeof(ℝ)) = 0.0
 testvalue(::typeof(ℝ₊)) = 1.0
 testvalue(::typeof(𝕀)) = 0.5
 
-struct IntegerBounds{L,U} <: AbstractDomain
+abstract type IntegerDomain <: AbstractDomain end
+
+struct IntegerNumbers <: IntegerDomain end
+
+Base.in(x, ::IntegerNumbers) = isinteger(x)
+
+const ℤ = IntegerNumbers()
+
+Base.show(io::IO, ::typeof(ℤ)) = print(io, "ℤ")
+
+Base.minimum(::IntegerNumbers) = static(-Inf)
+Base.maximum(::IntegerNumbers) = static(Inf)
+struct BoundedInts{L,U} <: IntegerDomain
     lower :: L
     upper :: U
 end
 
-Base.in(x, b::IntegerBounds) = isinteger(x) && b.lower ≤ x ≤ b.upper
+Base.in(x, b::BoundedInts) = x ∈ ℤ && b.lower ≤ x ≤ b.upper
 
-const ℤ = IntegerBounds(static(-Inf), static(Inf))
+Base.minimum(b::BoundedInts) = b.lower
+Base.maximum(b::BoundedInts) = b.upper
 
-Base.show(io::IO, ::typeof(ℤ)) = print(io, "ℤ")
-
-Base.minimum(::IntegerBounds{lo,hi}) where {lo,hi} = lo
-Base.maximum(::IntegerBounds{lo,hi}) where {lo,hi} = hi
-
-function Base.show(io::IO, b::IntegerBounds)
+function Base.show(io::IO, b::BoundedInts)
     io = IOContext(io, :compact => true)
     print(io, "ℤ[", b.lower, ":", b.upper, "]")
 end
 
-testvalue(::IntegerBounds) = min(b.lower, 0)
-
-Base.iterate(r::IntegerRange{lo,hi}) where {lo,hi} = iterate(lo:hi)
+testvalue(::BoundedInts) = min(b.lower, 0)
 
 function Base.getindex(::typeof(ℤ), r::AbstractUnitRange)
-    IntegerBounds(extrema(r)...)
+    BoundedInts(extrema(r)...)
 end
 
 
@@ -61,48 +80,48 @@ end
 ###########################################################
 # Simplex
 
-struct Simplex{D} <: AbstractDomain
-    dim::D # dimensionality as a manifold
-end
+# struct Simplex{D} <: AbstractDomain
+#     dim::D # dimensionality as a manifold
+# end
 
-projectto!(x, ::Simplex) = normalize!(x, 1)
+# projectto!(x, ::Simplex) = normalize!(x, 1)
 
-struct Sphere{D} <: AbstractDomain
-    dim::D # dimensionality as a manifold
-end
+# struct Sphere{D} <: AbstractDomain
+#     dim::D # dimensionality as a manifold
+# end
 
 
-projectto!(x, ::Sphere) = normalize!(x, 2)
-struct ZeroSet{F, G} <: AbstractDomain
-    f::F
-    ∇f::G
-end
+# projectto!(x, ::Sphere) = normalize!(x, 2)
+# struct ZeroSet{F, G} <: AbstractDomain
+#     f::F
+#     ∇f::G
+# end
 
-function zeroset(::Simplex)
-    f(x::AbstractVector{T}) where {T} = sum(x) - one(T)
-    ∇f(x::AbstractVector{T}) where {T} = Fill(one(T), axes(x))
-    ZeroSet(f, ∇f)
-end
+# function zeroset(::Simplex)
+#     f(x::AbstractVector{T}) where {T} = sum(x) - one(T)
+#     ∇f(x::AbstractVector{T}) where {T} = Fill(one(T), axes(x))
+#     ZeroSet(f, ∇f)
+# end
 
-function zeroset(::Sphere)
-    f(x::AbstractVector{T}) where {T} = (sum(xⱼ -> xⱼ^2, x) - one(T)) / 2
-    ∇f(x::AbstractVector{T}) where {T} = x
-    ZeroSet(f, ∇f)
-end
+# function zeroset(::Sphere)
+#     f(x::AbstractVector{T}) where {T} = (sum(xⱼ -> xⱼ^2, x) - one(T)) / 2
+#     ∇f(x::AbstractVector{T}) where {T} = x
+#     ZeroSet(f, ∇f)
+# end
 
-struct LebesgueCodimOne{D,T,O} <: AbstractMeasure
-    ndims ::D
-    ortho ::O
-end
+# struct LebesgueCodimOne{D,T,O} <: AbstractMeasure
+#     ndims ::D
+#     ortho ::O
+# end
 
-function logdensityof(d::Density{L1, L2}, x) where {L1<:LebesgueCodimOne, L2<:LebesgueCodimOne}
-    μ = d.μ
-    ν = d.base
-    μ.ndims == ν.ndims || return NaN
-    rank([μ.ortho ν.ortho]) == 1 || return NaN
-    return 0.0
-end
+# function logdensityof(d::Density{L1, L2}, x) where {L1<:LebesgueCodimOne, L2<:LebesgueCodimOne}
+#     μ = d.μ
+#     ν = d.base
+#     μ.ndims == ν.ndims || return NaN
+#     rank([μ.ortho ν.ortho]) == 1 || return NaN
+#     return 0.0
+# end
 
-struct LebesgueSimplex <: AbstractMeasure end
+# struct LebesgueSimplex <: AbstractMeasure end
 
-basemeasure(::LebesgueSimplex, x)
+# basemeasure(::LebesgueSimplex, x)
