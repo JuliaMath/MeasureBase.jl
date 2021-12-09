156c9909341a28229c5298e3356f1b6dfbbf6b43
Chad Scherrer <chad.scherrer@gmail.com>
2021-12-09 14:19:07 -0800

    some new stuff

---
+++ src/combinators/bind.jl
@@ -0,0 +1,30 @@
+struct Bind{M,K} <: AbstractMeasure
+    μ::M
+    k::K
+end
+
+export ↣
+
+"""
+If 
+- μ is an `AbstractMeasure` or satisfies the Measure interface, and
+- k is a function taking values from the support of μ and returning a measure
+
+Then `μ ↣ k` is a measure, called a *monadic bind*. In a
+probabilistic programming language like Soss.jl, this could be expressed as
+
+Note that bind is usually written `>>=`, but this symbol is unavailable in Julia.
+
+```
+bind = @model μ,k begin 
+    x ~ μ
+    y ~ k(x)
+    return y
+end
+```
+
+See also `bind` and `Bind`
+"""
+↣(μ, k) = bind(μ, k)
+
+bind(μ, k) = Bind(μ, k)
---
+++ src/kleisli.jl
@@ -0,0 +1,89 @@
+# TODO: Dangerous to export this - let's not
+abstract type AbstractKleisli <: AbstractMeasure end
+
+struct ParameterizedKleisli{F,N,T} <: AbstractKleisli
+    f::F
+    param_maps::NamedTuple{N,T}
+
+    ParameterizedKleisli(::Type{F}, param_maps::NamedTuple{N,T}) where {F,N,T} =
+        new{Type{F},N,T}(F, param_maps)
+    ParameterizedKleisli(f::F, param_maps::NamedTuple{N,T}) where {F,N,T} =
+        new{F,N,T}(f, param_maps)
+end
+
+"""
+    kleisli(f, M)
+    kleisli((f1, f2, ...), M)
+
+A kleisli `κ = kleisli(f, m)` returns a wrapper around
+a function `f` giving the parameters for a measure of type `M`,
+such that `κ(x) = M(f(x)...)`
+respective `κ(x) = M(f1(x), f2(x), ...)`
+
+If the argument is a named tuple `(;a=f1, b=f1)`, `κ(x)` is defined as
+`M(;a=f(x),b=g(x))`.
+
+# Reference
+
+* https://en.wikipedia.org/wiki/Markov_kleisli
+"""
+function kleisli end
+
+
+# kleisli(Normal) do x
+#     (μ=x,σ=x^2)
+# end
+
+kleisli(f, ::Type{M}) where {M} = kleisli(M, f)
+
+mapcall(t, x) = map(func -> func(x), t)
+
+# (k::Kleisli{Type{P},<:Tuple})(x) where {P<:ParameterizedMeasure} = k.f(mapcall(k.param_maps, x)...)
+
+(k::ParameterizedKleisli)(x) where {M} = k.f(; mapcall(k.param_maps, x)...)
+
+(k::ParameterizedKleisli)(x...) = k(x)
+
+function (k::ParameterizedKleisli)(x::Tuple)
+    k.f(NamedTuple{k.param_maps}(x))
+end
+
+
+"""
+For any `k::Kleisli`, `basekleisli` is expected to satisfy
+```
+basekleisli(k)(p) == (basemeasure ∘ k)(p)
+```
+
+The main purpose of `basekleisli` is to make it efficient to compute
+```
+basemeasure(d::ProductMeasure) = productmeasure(basekleisli(d.f), d.xs)
+```
+"""
+function basekleisli end
+
+# TODO: Find a way to do better than this
+basekleisli(f) = basemeasure ∘ f
+
+basekleisli(k::ParameterizedKleisli) = kleisli(basekleisli(k.f), k.param_maps)
+
+basekleisli(f::Returns) = Returns(basemeasure(f.value))
+
+# export kleisliize
+
+# function kleisliize(μ::M) where {N, M <: ParameterizedMeasure{N}}
+#     C = constructorof(M)
+#     (Kleisli{C,}(NamedTuple{N}, ), values(getfield(μ, :par)))
+# end
+
+function Base.show(io::IO, μ::AbstractKleisli)
+    io = IOContext(io, :compact => true)
+    Pretty.pprint(io, μ)
+end
+
+function Pretty.quoteof(k::ParameterizedKleisli)
+    qf = Pretty.quoteof(k.f)
+    qg = Pretty.quoteof(k.param_maps)
+    :(ParameterizedKleisli($qf, $qg))
+end
+
