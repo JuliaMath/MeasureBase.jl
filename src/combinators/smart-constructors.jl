a1ee588af30efa9370cc62122f715deb65648bc4
Chad Scherrer <chad.scherrer@gmail.com>
2021-12-09 18:55:17 -0800

    bugfix

--- src/combinators/smart-constructors.jl
+++ src/combinators/smart-constructors.jl
@@ -17,23 +17,8 @@ end
 ###############################################################################
 # PowerMeaure
 
-function powermeasure(μ::WeightedMeasure, dims::NTuple{N,I}) where {N,I<:AbstractArray}
-    k = mapreduce(length, *, dims...) * μ.logweight
-    return weightedmeasure(k, μ.base^dims)
-end
-
-function powermeasure(μ::WeightedMeasure, dims::NTuple{N,I}) where {N,I<:Base.Generator}
-    k = mapreduce(length, *, dims...) * μ.logweight
-    return weightedmeasure(k, μ.base^dims)
-end
-
 function powermeasure(μ::WeightedMeasure, dims::NTuple{N,I}) where {N,I}
-    k = mapreduce(length, *, dims...) * μ.logweight
-    return weightedmeasure(k, μ.base^dims)
-end
-
-function powermeasure(μ::WeightedMeasure, dims::NTuple{N,I}) where {N,I}
-    k = mapreduce(length, *, dims...) * μ.logweight
+    k = mapreduce(length, *, dims) * μ.logweight
     return weightedmeasure(k, μ.base^dims)
 end
 
