8a1411c163a5c09d85c200b073ebe98ece98784f
Chad Scherrer <chad.scherrer@gmail.com>
2021-12-12 07:48:08 -0800

    Dirac bugfix

--- src/primitives/dirac.jl
+++ src/primitives/dirac.jl
@@ -19,7 +19,7 @@ end
 
 basemeasure(d::Dirac) = CountingMeasure()
 
-tbasemeasure_type(::Type{Dirac}) = CountingMeasure
+tbasemeasure_type(::Type{Dirac{X}}) where {X} = CountingMeasure
 
 density_def(μ::Dirac, x) = x == μ.x
 
