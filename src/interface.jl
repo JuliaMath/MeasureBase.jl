5fc1eeacc089112bd72f6edb47fab6af02e66efd
Chad Scherrer <chad.scherrer@gmail.com>
2021-12-12 10:04:41 -0800

    update interface

--- src/interface.jl
+++ src/interface.jl
@@ -15,28 +15,31 @@ function test_interface(μ::M) where {M}
     # basemeasure_depth
     @testset "$μ" begin
         static_depth = @inferred basemeasure_depth(μ) 
-        
+
         @test static_depth == tbasemeasure_depth(M)
         dynamic_depth = dynamic_basemeasure_depth(μ)
-        
+
         if static_depth > dynamic_depth
             @warn "basemeasure_depth($μ) greater than requirement, could add some overhead"
         end
         @test static_depth ≥ dynamic_depth
-    
+
         ###########################################################################
         # testvalue, logdensityof
-    
+
         x = @inferred testvalue(μ)
         β = @inferred basemeasure(μ)
-    
-        ℓμ = @inferred logdensityof(μ, x)
-        ℓβ = @inferred logdensityof(β, x)
+
+        @inferred logdensityof(μ, x)
+        @inferred logdensityof(β, x)
+        ℓμ = logdensityof(μ, x)
+        ℓβ = logdensityof(β, x)
+
         @test ℓμ ≈ logdensity_def(μ, x) + ℓβ
-    
+
         ###########################################################################
         # basemeasure_type
-    
+
         @test @inferred tbasemeasure_type(M) == @inferred basemeasure_type(μ)
         @test basemeasure_type(μ) == (typeof ∘ basemeasure)(μ)
     end
