61f64408a9596c92545cedaa30e3c10630107557
Chad Scherrer <chad.scherrer@gmail.com>
2021-12-14 13:58:27 -0800

    update SpikeMixture

--- src/combinators/spikemixture.jl
+++ src/combinators/spikemixture.jl
@@ -2,39 +2,37 @@ export SpikeMixture
 
 # TODO: Add `AbstractSuperposition <: AbstractMeasure`, and make SpikeMixture a
 # subtype of this 
-struct SpikeMixture{T,S} <: AbstractMeasure
-    m::T # parent
-    w::S # relative weight of parent
+struct SpikeMixture{M,W,S} <: AbstractMeasure
+    m::M   # parent
+    w::W   # parent weight
+    s::S   # spike weight
 end
 
+SpikeMixture(μ,w) = SpikeMixture(μ, w, static(1.0) - w) 
+
 function Pretty.tile(μ::SpikeMixture)
-    result = Pretty.tile(μ.w)
-    result *= Pretty.tile(μ.m)
-    result *= Pretty.literal(" + ")
-    result *= Pretty.tile(1 - μ.w)
-    result *= Pretty.literal("Dirac(0)")
-    return result
+    Pretty.list_layout(Pretty.tile.([μ.m, μ.w]), prefix="SpikeMixture")
 end
 
 # TODO: Should this base measure be local? 
 @inline function basemeasure(μ::SpikeMixture)
     # Compare formula (1.4) in Joris Bierkens, Sebastiano Grazzi, Frank van der Meulen, Moritz Schauer:
     # Sticky PDMP samplers for sparse and local inference problems. 2020. [https://arxiv.org/abs/2103.08478].
-    SpikeMixture(basemeasure(μ.m), μ.w)
+    SpikeMixture(basemeasure(μ.m), static(1.0), static(1.0))
 end
 
-tbasemeasure_depth(::Type{SpikeMixture{T,S}}) where {T,S} = static(1) + tbasemeasure_depth(T)
+# tbasemeasure_depth(::Type{SpikeMixture{M,W}}) where {M,W} = static(1) + tbasemeasure_depth(M)
 
 # basemeasure_type(::Type{SpikeMixture{T,S}}) where {T,S} = SpikeMixture{}
 
-function tbasemeasure_type(::Type{SpikeMixture{M, T}}) where {M,T}
+function tbasemeasure_type(::Type{SpikeMixture{M,W,S}}) where {M,W,S}
     B = tbasemeasure_type(M)
-    SpikeMixture{B, T}
+    SpikeMixture{B,StaticFloat64{1.0},StaticFloat64{1.0}}
 end
 
 @inline function logdensity_def(μ::SpikeMixture, x)
     if iszero(x)
-        return log1p(-μ.w) 
+        return log(μ.s) 
     else
         return log(μ.w) + logdensity_def(μ.m, x)
     end
