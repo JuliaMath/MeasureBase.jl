328f569200fef85e64f1311c6423e9766c99c8be
Chad Scherrer <chad.scherrer@gmail.com>
2022-06-03 08:37:24 -0700

    zero(x) ≤ x ≤ one(x)

--- src/standard/stduniform.jl
+++ src/standard/stduniform.jl
@@ -2,7 +2,7 @@ struct StdUniform <: AbstractMeasure end
 
 export StdUniform
 
-insupport(d::StdUniform, x) = 0 ≤ x ≤ 1
+insupport(d::StdUniform, x) = zero(x) ≤ x ≤ one(x)
 
 @inline logdensity_def(::StdUniform, x) = zero(x)
 @inline basemeasure(::StdUniform) = Lebesgue()
