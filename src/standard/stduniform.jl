ac9b6521d802b4679797d6fcc2aa2ab1678ef631
Chad Scherrer <chad.scherrer@gmail.com>
2022-06-03 07:36:46 -0700

    drop duplicate code

--- src/standard/stduniform.jl
+++ src/standard/stduniform.jl
@@ -8,14 +8,3 @@ insupport(d::StdUniform, x) = 0 ≤ x ≤ 1
 @inline basemeasure(::StdUniform) = Lebesgue()
 
 Base.rand(rng::Random.AbstractRNG, ::Type{T}, ::StdUniform) where {T} = randn(rng, T)
-
-struct StdUniform <: AbstractMeasure end
-
-export StdUniform
-
-insupport(d::StdUniform, x) = zero(x) ≤ x ≤ one(x)
-
-@inline logdensity_def(::StdUniform, x) = -x^2 / 2
-@inline basemeasure(::StdUniform) = LebesgueMeasure()
-
-Base.rand(rng::Random.AbstractRNG, ::Type{T}, ::StdUniform) where {T} = rand(rng, T)
