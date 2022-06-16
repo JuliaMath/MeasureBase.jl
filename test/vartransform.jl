5a16befbba4f25329b8238c3d364eb3ef1d3db0d
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-16 20:14:43 +0200

    FIXUP vartransform tests

--- test/vartransform.jl
+++ test/vartransform.jl
@@ -6,15 +6,18 @@ using InverseFunctions: inverse
 using ChangesOfVariables: with_logabsdet_jacobian
 
 @testset "vartransform" begin
+    supertype(x::Real) = Real
+    supertype(x::AbstractArray{T,N}) where {T,N} = AbstractArray{T,N}
+
     function test_transform_and_back(ν, μ)
-        @testset "vartransform powers from $(nameof(typeof(μ))) to $(ν)" begin
+        @testset "vartransform $μ to $ν0" begin
             x = rand(μ)
             @test !(@inferred(vartransform(ν, μ)(x)) isa NoVarTransform)
             f = vartransform(ν, μ)
             y = f(x)
             @test @inferred(inverse(f)(y)) ≈ x
-            @test @inferred(with_logabsdet_jacobian(f, x)) isa Tuple{typeof(y),Real}
-            @test @inferred(with_logabsdet_jacobian(inverse(f), y)) isa Tuple{typeof(x),Real}
+            @test @inferred(with_logabsdet_jacobian(f, x)) isa Tuple{supertype(y),Real}
+            @test @inferred(with_logabsdet_jacobian(inverse(f), y)) isa Tuple{supertype(x),Real}
             y2, ladj_fwd = with_logabsdet_jacobian(f, x)
             x2, ladj_inv = with_logabsdet_jacobian(inverse(f), y)
             @test x ≈ x2
@@ -24,15 +27,15 @@ using ChangesOfVariables: with_logabsdet_jacobian
         end
     end
 
-    for μ in [StdUniform(), StdExponential(), StdLogistic()], ν in [StdUniform(), StdExponential(), StdLogistic()]
-        @testset "vartransform powers of $(nameof(typeof(μ))) to $(ν)" begin
-            test_transform_and_back(ν, μ)
-            test_transform_and_back(ν, μ^1)
-            test_transform_and_back(ν^1, μ)
-            test_transform_and_back(ν^3, μ^3)
-            test_transform_and_back(ν^(2,3,2), μ^(3,4))
-            @test_throws ArgumentError vartransform(ν, μ)(rand(μ^12))
-            @test_throws ArgumentError vartransform(ν^3, μ^3)(rand(μ^(3,4)))
+    for μ0 in [StdUniform(), StdExponential(), StdLogistic()], ν0 in [StdUniform(), StdExponential(), StdLogistic()]
+        @testset "vartransform (powers of) $(nameof(typeof(μ0))) to $(nameof(typeof(ν0)))" begin
+            test_transform_and_back(ν0, μ0)
+            test_transform_and_back(ν0, μ0^1)
+            test_transform_and_back(ν0^1, μ0)
+            test_transform_and_back(ν0^3, μ0^3)
+            test_transform_and_back(ν0^(2,3,2), μ0^(3,4))
+            @test_throws ArgumentError vartransform(ν0, μ0)(rand(μ0^12))
+            @test_throws ArgumentError vartransform(ν0^3, μ0^3)(rand(μ0^(3,4)))
         end
     end
 end
