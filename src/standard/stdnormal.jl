1b9cec91087fd31cc2bea310b5262e6190be3a6e
Chad Scherrer <chad.scherrer@gmail.com>
2022-09-11 16:54:51 -0700

    no need to qualify

--- src/standard/stdnormal.jl
+++ src/standard/stdnormal.jl
@@ -1,18 +1,18 @@
-struct StdNormal <: MeasureBase.StdMeasure end
+struct StdNormal <: StdMeasure end
 
 export StdNormal
 
-@inline MeasureBase.insupport(d::StdNormal, x) = true
+@inline insupport(d::StdNormal, x) = true
 
-@inline MeasureBase.logdensity_def(::StdNormal, x) = -x^2 / 2
-@inline MeasureBase.basemeasure(::StdNormal) = WeightedMeasure(static(-0.5 * log2π), Lebesgue(ℝ))
+@inline logdensity_def(::StdNormal, x) = -x^2 / 2
+@inline basemeasure(::StdNormal) = WeightedMeasure(static(-0.5 * log2π), Lebesgue(ℝ))
 
-@inline MeasureBase.getdof(::StdNormal) = static(1)
+@inline getdof(::StdNormal) = static(1)
 
-@inline MeasureBase.transport_def(::StdUniform, μ::StdNormal, x) = StatsFuns.normcdf(x)
-@inline MeasureBase.transport_def(::StdNormal, μ::StdUniform, x) = StatsFuns.norminvcdf(x)
+@inline transport_def(::StdUniform, μ::StdNormal, x) = StatsFuns.normcdf(x)
+@inline transport_def(::StdNormal, μ::StdUniform, x) = StatsFuns.norminvcdf(x)
 
 @inline Base.rand(rng::Random.AbstractRNG, ::Type{T}, ::StdNormal) where {T} = randn(rng, T)
 
 
-@inline MeasureBase.StdMeasure(::typeof(randn)) = StdNormal()
+@inline StdMeasure(::typeof(randn)) = StdNormal()
