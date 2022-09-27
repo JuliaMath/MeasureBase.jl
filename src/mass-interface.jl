2393222f718e7a9a4df09c821bc2173d20b15690
Chad Scherrer <chad.scherrer@gmail.com>
2022-09-27 09:54:38 -0700

    docstrings

--- src/mass-interface.jl
+++ src/mass-interface.jl
@@ -4,8 +4,18 @@ import Base
 
 abstract type AbstractUnknownMass <: Number end
 
+"""
+    struct UnknownFiniteMass <: AbstractUnknownMass end
+
+See `massof`
+"""
 struct UnknownFiniteMass <: AbstractUnknownMass end
 
+"""
+    struct UnknownMass <: AbstractUnknownMass end
+
+See `massof`
+"""
 struct UnknownMass <: AbstractUnknownMass end
 
 for T in (:UnknownFiniteMass, :UnknownMass)
@@ -28,6 +38,33 @@ for op in (:+, :*)
 end
 
 export massof
+
+"""
+    massof(m)
+
+Get the _mass_ of a measure - that is, integrate the measure over its support.
+
+`massof` 
+
+----------
+
+    massof(m, dom)
+
+Integrate the measure `m` over the "domain" `dom`. Note that domains are not
+defined universally, but may be specific to a given measure. If `m` is
+`<:AbstractMeasure`, users can also write `m(dom)`. For new measures, users
+should *not* add new "call" methods, but instead extend `MeasureBase.massof`.
+
+
+For example, for many univariate measures `m` with `rootmeasure(m) ==
+LebesgueBase()`, users can call `massof(m, a_b)` where
+`a_b::IntervalSets.Interval`.
+
+`massof` often returns a `Real`. But in many cases we may only know the mass is
+finite, or we may know nothing at all about it. For these cases, it will return
+`UnknownFiniteMass` or `UnknownMass`, respectively. When no `massof` method
+exists, it defaults to `UnknownMass`.
+"""
 massof(m::AbstractMeasure) = UnknownMass(m)
 
 struct NormalizedMeasure{P,M} <: AbstractMeasure
