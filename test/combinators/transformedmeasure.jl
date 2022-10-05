72c4307656a702352a0305ddbcdee3db3f4934c5
Chad Scherrer <chad.scherrer@gmail.com>
2022-10-05 13:55:07 -0700

    more work on tests

--- test/combinators/transformedmeasure.jl
+++ test/combinators/transformedmeasure.jl
@@ -6,18 +6,33 @@ using MeasureBase: transport_to, unsafe_logdensityof
 import Statistics: var
 using DensityInterface: logdensityof
 using LogExpFunctions
+using SpecialFunctions: erfc, erfcinv
+import InverseFunctions: inverse
+
+
+Φ(z) = erfc(-z * invsqrt2)/2
+Φinv(p) = -erfcinv(2*p) * sqrt2
+
+inverse(::typeof(Φ)) = Φinv
+inverse(::typeof(Φinv)) = Φ
 
 var(::StdNormal) = 1.0
 var(::StdExponential) = 1.0
 var(::StdUniform) = 1/12
+var(::StdLogistic) = π^2/3
 
 @testset "transformedmeasure.jl" begin
     for (f, μ, ν_ref) in [
         ((-) ∘ log1p ∘ (-), StdUniform(), StdExponential())
         ((-) ∘ log, StdUniform(), StdExponential())
+        (exp ∘ (-), StdExponential(), StdUniform())
         (logit, StdUniform(), StdLogistic())
         (logistic, StdLogistic(), StdUniform())
+        # (Φ, StdNormal(), StdUniform())
+        # (Φinv, StdUniform(), StdNormal())
         ]
+
+        finv = inverse(f)
 
         @testset "pushfwd($f, $μ)" begin
             @test @inferred(pushfwd(f, μ)) isa PushforwardMeasure
@@ -29,7 +44,11 @@ var(::StdUniform) = 1/12
 
             @test isapprox(var(rand(ν^(10^5))), var(ν_ref), rtol = 0.05)
 
-            @test transport_to(StdLogistic(), ν)(y) ≈ transport_to(StdLogistic(), ν)(y)
+            @test abs(transport_to(StdLogistic(), ν)(y)) ≈ abs(transport_to(StdLogistic(), ν_ref)(y))
+        end
+
+        @testset "pushfwd($finv, pushfwd($f, μ))" begin
+            # TODO
         end
     end
 end
