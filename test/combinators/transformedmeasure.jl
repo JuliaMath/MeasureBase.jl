6b2bfd56ddf572901d01e410ad8c2085db4a6c97
Chad Scherrer <chad.scherrer@gmail.com>
2022-10-05 16:34:51 -0700

    add some more failing tests

--- test/combinators/transformedmeasure.jl
+++ test/combinators/transformedmeasure.jl
@@ -12,8 +12,8 @@ import InverseFunctions: inverse, FunctionWithInverse
 using IrrationalConstants: invsqrt2, sqrt2
 import ChangesOfVariables: with_logabsdet_jacobian
 
-Φ(z) = erfc(-z * invsqrt2) / 2
-Φinv(p) = -erfcinv(2 * p) * sqrt2
+Φ(z) = erfc(-z * invsqrt2)/2
+Φinv(p) = -erfcinv(2*p) * sqrt2
 
 with_logabsdet_jacobian(f::FunctionWithInverse, x) = with_logabsdet_jacobian(f.f, x)
 
@@ -29,22 +29,24 @@ inverse(::typeof(Φinv)) = Φ
 
 var(::StdNormal) = 1.0
 var(::StdExponential) = 1.0
-var(::StdUniform) = 1 / 12
-var(::StdLogistic) = π^2 / 3
+var(::StdUniform) = 1/12
+var(::StdLogistic) = π^2/3
 
 function test_pushfwd(f, μ, ν_ref)
     @testset "pushfwd($f, $μ)" begin
         @inferred(pushfwd(f, μ))
         ν = pushfwd(f, μ)
 
+        test_transport(ν, ν_ref)
+        test_transport(ν_ref, ν)
+        
         y = rand(ν_ref)
         @test @inferred(logdensityof(ν, y)) ≈ logdensityof(ν_ref, y)
         @test @inferred(unsafe_logdensityof(ν, y)) ≈ unsafe_logdensityof(ν_ref, y)
 
         @test isapprox(var(rand(ν^(10^5))), var(ν_ref), rtol = 0.05)
 
-        @test abs(transport_to(StdLogistic(), ν)(y)) ≈
-              abs(transport_to(StdLogistic(), ν_ref)(y))
+        @test abs(transport_to(StdLogistic(), ν)(y)) ≈ abs(transport_to(StdLogistic(), ν_ref)(y))
     end
 end
 
@@ -59,7 +61,7 @@ end
         (Φ, StdNormal(), StdUniform())
         (Φinv, StdUniform(), StdNormal())
     ]
-
+    
     for (f, μ, ν_ref) in triples
         test_pushfwd(f, μ, ν_ref)
     end
