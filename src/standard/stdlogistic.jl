b0df9858f3509748b44620ae8324a885e2efabe3
Chad Scherrer <chad.scherrer@gmail.com>
2022-10-12 08:54:23 -0700

    make transport_def depend on smf(inv)

--- src/standard/stdlogistic.jl
+++ src/standard/stdlogistic.jl
@@ -7,8 +7,8 @@ export StdLogistic
 @inline logdensity_def(::StdLogistic, x) = (u = -abs(x); u - 2 * log1pexp(u))
 @inline basemeasure(::StdLogistic) = LebesgueBase()
 
-@inline transport_def(::StdUniform, μ::StdLogistic, x) = logistic(x)
-@inline transport_def(::StdLogistic, μ::StdUniform, x) = logit(x)
+@inline transport_def(::StdUniform, μ::StdLogistic, x) = smf(StdLogistic(), x)
+@inline transport_def(::StdLogistic, μ::StdUniform, p) = smfinv(StdLogistic(), p)
 
 @inline function Base.rand(rng::Random.AbstractRNG, ::Type{T}, ::StdLogistic) where {T}
     logit(rand(rng, T))
