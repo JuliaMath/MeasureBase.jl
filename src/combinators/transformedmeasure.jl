97cbb33482ae7b95dcfaab742b880837753bfd7a
Chad Scherrer <chad.scherrer@gmail.com>
2022-10-15 15:33:04 -0700

    fix docstring

--- src/combinators/transformedmeasure.jl
+++ src/combinators/transformedmeasure.jl
@@ -19,8 +19,12 @@ function parent(::AbstractTransformedMeasure) end
 export PushforwardMeasure
 
 """
-    struct PushforwardMeasure{F,I,MU,VC<:TransformVolCorr} <:
-        AbstractPushforward f :: F finv :: I origin :: MU volcorr :: VC end
+    struct PushforwardMeasure{F,I,M,VC<:TransformVolCorr} <: AbstractPushforward
+        f :: F
+        finv :: I
+        origin :: M
+        volcorr :: VC
+    end
 
     Users should not call `PushforwardMeasure` directly. Instead call or add
     methods to `pushfwd`.
