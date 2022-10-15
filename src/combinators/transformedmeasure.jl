18a9f99d938f79b49e46138df241a285d958f04e
Chad Scherrer <chad.scherrer@gmail.com>
2022-10-15 15:57:51 -0700

    drop redundant method

--- src/combinators/transformedmeasure.jl
+++ src/combinators/transformedmeasure.jl
@@ -127,10 +127,6 @@ function pushfwd(f, μ::PushforwardMeasure, volcorr::TransformVolCorr = WithVolC
     _pushfwd_of_pushfwd(f, μ, μ.volcorr, volcorr)
 end
 
-function pushfwd(f, μ::PushforwardMeasure, ::WithVolCorr)
-    _pushfwd_of_pushfwd(f, μ, μ.volcorr, WithVolCorr())
-end
-
 # Either both WithVolCorr or both NoVolCorr, so we can merge them
 function _pushfwd_of_pushfwd(f, μ::PushforwardMeasure, ::V, v::V) where {V}
     pushfwd(fchain((μ.f, f)), μ.origin, v)
