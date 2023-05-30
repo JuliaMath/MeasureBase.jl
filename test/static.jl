b1544406050a339fb42c1094ef8f20850c5978f5
Oliver Schulz <oschulz@mpp.mpg.de>
2023-05-30 18:19:19 +0200

    Add tests for static integer and range functions

---
+++ test/static.jl
@@ -0,0 +1,31 @@
+using Test
+
+import MeasureBase
+
+import Static
+using Static: static
+import FillArrays
+
+@testset "static" begin
+    @test 2 isa MeasureBase.IntegerLike
+    @test static(2) isa MeasureBase.IntegerLike
+    @test true isa MeasureBase.IntegerLike
+    @test static(true) isa MeasureBase.IntegerLike
+    
+    @test @inferred(MeasureBase.one_to(7)) isa Base.OneTo
+    @test @inferred(MeasureBase.one_to(7)) == 1:7
+    @test @inferred(MeasureBase.one_to(static(7))) isa Static.SOneTo
+    @test @inferred(MeasureBase.one_to(static(7))) == static(1):static(7)
+
+    @test @inferred(MeasureBase.fill_with(4.2, (7,))) == FillArrays.Fill(4.2, 7)
+    @test @inferred(MeasureBase.fill_with(4.2, (static(7),))) == FillArrays.Fill(4.2, 7)
+    @test @inferred(MeasureBase.fill_with(4.2, (3, static(7)))) == FillArrays.Fill(4.2, 3, 7)
+    @test @inferred(MeasureBase.fill_with(4.2, (3:7,))) == FillArrays.Fill(4.2, (3:7,))
+    @test @inferred(MeasureBase.fill_with(4.2, (static(3):static(7),))) == FillArrays.Fill(4.2, (3:7,))
+    @test @inferred(MeasureBase.fill_with(4.2, (3:7, static(2):static(5)))) == FillArrays.Fill(4.2, (3:7, 2:5))
+
+    @test MeasureBase.maybestatic_length(MeasureBase.one_to(7)) isa Int
+    @test MeasureBase.maybestatic_length(MeasureBase.one_to(7)) == 7
+    @test MeasureBase.maybestatic_length(MeasureBase.one_to(static(7))) isa Static.StaticInt
+    @test MeasureBase.maybestatic_length(MeasureBase.one_to(static(7))) == static(7)
+end
