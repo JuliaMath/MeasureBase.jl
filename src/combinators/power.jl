165708f6ce0b7dd1d46bcb17a7871ef334bc7fa8
Chad Scherrer <chad.scherrer@gmail.com>
2023-05-30 06:56:21 -0700

    format

--- src/combinators/power.jl
+++ src/combinators/power.jl
@@ -29,7 +29,9 @@ end
 
 # ToDo: Make rand return static arrays for statically-sized power measures.
 
-_cartidxs(axs::Tuple{Vararg{<:AbstractUnitRange,N}}) where {N} = CartesianIndices(map(_dynamic, axs))
+function _cartidxs(axs::Tuple{Vararg{<:AbstractUnitRange,N}}) where {N}
+    CartesianIndices(map(_dynamic, axs))
+end
 
 function Base.rand(
     rng::AbstractRNG,
@@ -47,13 +49,12 @@ function Base.rand(rng::AbstractRNG, ::Type{T}, d::PowerMeasure) where {T}
     end
 end
 
-@inline _pm_axes(sz::Tuple{Vararg{<:IntegerLike,N}}) where N = map(one_to, sz)
-@inline _pm_axes(axs::Tuple{Vararg{<:AbstractUnitRange,N}}) where N = axs
+@inline _pm_axes(sz::Tuple{Vararg{<:IntegerLike,N}}) where {N} = map(one_to, sz)
+@inline _pm_axes(axs::Tuple{Vararg{<:AbstractUnitRange,N}}) where {N} = axs
 
 @inline function powermeasure(x::T, sz::Tuple{Vararg{<:Any,N}}) where {T,N}
     PowerMeasure(x, _pm_axes(sz))
 end
-
 
 marginals(d::PowerMeasure) = fill_with(d.parent, d.axes)
 
@@ -84,10 +85,7 @@ end
     end
 end
 
-@inline function logdensity_def(
-    d::PowerMeasure{M,Tuple{Static.SOneTo{N}}},
-    x,
-) where {M,N}
+@inline function logdensity_def(d::PowerMeasure{M,Tuple{Static.SOneTo{N}}}, x) where {M,N}
     parent = d.parent
     sum(1:N) do j
         @inbounds logdensity_def(parent, x[j])
--- src/static.jl
+++ src/static.jl
@@ -5,7 +5,6 @@ Equivalent to `Union{Integer,Static.StaticInt}`.
 """
 const IntegerLike = Union{Integer,Static.StaticInt}
 
-
 """
     MeasureBase.one_to(n::IntegerLike)
 
@@ -15,13 +14,11 @@ Returns an instance of `Base.OneTo` or `Static.SOneTo`, depending
 on the type of `n`.
 """
 @inline one_to(n::Integer) = Base.OneTo(n)
-@inline one_to(::Static.StaticInt{N}) where N = Static.SOneTo{N}()
-
+@inline one_to(::Static.StaticInt{N}) where {N} = Static.SOneTo{N}()
 
 _dynamic(x::Number) = dynamic(x)
-_dynamic(::Static.SOneTo{N}) where N = Base.OneTo(N)
+_dynamic(::Static.SOneTo{N}) where {N} = Base.OneTo(N)
 _dynamic(r::AbstractUnitRange) = minimum(r):maximum(r)
-
 
 """
     MeasureBase.fill_with(x, sz::NTuple{N,<:IntegerLike}) where N
@@ -32,7 +29,9 @@ Returns an instance of `FillArrays.Fill`.
 """
 function fill_with end
 
-@inline fill_with(x::T, sz::Tuple{Vararg{<:IntegerLike,N}}) where {T,N} = fill_with(x, map(one_to, sz))
+@inline function fill_with(x::T, sz::Tuple{Vararg{<:IntegerLike,N}}) where {T,N}
+    fill_with(x, map(one_to, sz))
+end
 
 @inline function fill_with(x::T, axs::Tuple{Vararg{<:AbstractUnitRange,N}}) where {T,N}
     # While `FillArrays.Fill` (mostly?) works with axes that are static unit
@@ -43,7 +42,6 @@ function fill_with end
     FillArrays.Fill(x, dyn_axs)
 end
 
-
 """
     MeasureBase.dslength(x)::IntegerLike
 
@@ -51,8 +49,9 @@ Returns the length of `x` as a dynamic or static integer.
 """
 dslength(x) = length(x)
 dslength(x::AbstractUnitRange) = length(x)
-dslength(::Static.OptionallyStaticUnitRange{StaticInt{A}, StaticInt{B}}) where {A,B} = StaticInt{B - A + 1}()
-
+function dslength(::Static.OptionallyStaticUnitRange{StaticInt{A},StaticInt{B}}) where {A,B}
+    StaticInt{B - A + 1}()
+end
 
 """
     MeasureBase.dssize(x)::Tuple{Vararg{IntegerLike}}
