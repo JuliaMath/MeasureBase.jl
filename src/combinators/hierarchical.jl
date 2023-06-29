8bf135d88bb5cfd7117f3ecf7831407710c72863
Oliver Schulz <oschulz@mpp.mpg.de>
2023-06-29 17:03:20 +0200

    STASH

--- src/combinators/hierarchical.jl
+++ src/combinators/hierarchical.jl
@@ -1,79 +1,103 @@
-export HierarchicalMeasure
+export Bind
 
 """
-    struct HierarchicalMeasure{F,M<:AbstractMeasure,G} <: AbstractMeasure
+    struct MeasureBase.Bind{F,M<:AbstractMeasure,G} <: AbstractMeasure
 
-Represents a hierarchical measure.
+Represents a monatic bind resp. a mbind in general.
 
-User code should not instantiate `HierarchicalMeasure` directly, use
-[`hierarchical_measure`](@ref) instead.
+User code should not create instances of `Bind` directly, but should call
+[`mbind`](@ref) instead.
 """
-struct HierarchicalMeasure{F,M<:AbstractMeasure,G} <: AbstractMeasure
+struct Bind{F,M<:AbstractMeasure,G} <: AbstractMeasure
     f_kernel::F
-    μ_primary::M
+    m_primary::M
     f_combine::G
 end
 
 
 @doc raw"""
-    hierarchical_measure(k, μ_primary::AbstractMeasure, f_combine)
+    mbind(f_β, α::AbstractMeasure, f_c = second)
 
-Construct a hierarchical measure from a transition kernel function
-`f_kernel`, a measure `μ_primary` and a function `f_combine`.
+Constructs a monadic bind resp. a hierarchical measure from a transition
+kernel function `f_β`, a primary measure `α` and a variate combination
+function `f_c`.
 
-`f_kernel` must be a function that maps a variate `x_primary` of
-`μ_primary` to a dependent secondary measure `μ_secondary = f(x_primary)).
-`y = f_combine(x_primary, x_secondary)` must map variates from the primary
-and the dependent secondary measure to a combined value.
+`f_β` must be a function that maps a point `a` from the space of measure
+`α` to a dependent measure `β_a = f_β(a)`. `ab = f_combine(a, b)` must
+`a` and e
+primary and a variates `b` of the dependent secondary measure `β_a` to
+a combined value `ab`.
 
 A measure
 
 ```julia
-`μ = hierarchical_measure(f_c, α, f_β)`
+`μ = mbind(f_c, α, f_β)`
 ```
 
-has the mathethematical interpretation (using the notation `β_a = f_β(a)`):
+has the mathethematical interpretation
 
 ```math
 \mu(f_c(A, B)) = \int_A \beta_a(B)\, \mathrm{d}\, \alpha(a) 
 ```
 
-Comutationally, `x = rand(μ)` is equivalent to
+Without the default `fc = second` this becomes
 
-```julia
-x_primary = rand(μ_primary)
-μ_secondary = f_kernel(x_primary)
-x_secondary = rand(μ_secondary)
-x = f_combine(x_primary, x_secondary)
+```math
+\mu(B) = \int_A \beta_a(B)\, \mathrm{d}\, \alpha(a) 
 ```
 
-and `mbind(f_β, α)` is equivalent to:
+which is equivalent to a monatic bind, when viewing measures as monads.
+
+Comutationally, `ab = rand(μ)` is equivalent to
 
 ```julia
-pushfwd((a, b) -> b, hierarchical_measure(=>, α, tuple))
+a = rand(μ_primary)
+β_a = f_β(a)
+b = rand(β_a)
+ab = f_combine(a, b)
 ```
 
-Possible choices for `f_combine` are `=>`/`Pair` or `tuple` (these work for
+Densities on hierarchical measures can only be evaluated if `ab = f_c(a, b)`
+can be unambiguously split into `a` and `b` again. This is currently
+implemented for `f_c` that is either `=>`/`Pair` or `tuple` (these work for
 any combination of variate types), `vcat` (for tuple- or vector-like
-variates) and `merge` (e.g. for `NamedTuple` variates).
+variates) and `merge` (`NamedTuple` variates).
+[`MeasureBase.split_point(::typeof(f_c), α)`](@ref) can be specialized to
+support other choices for `f_c`.
 """
-@inline function hierarchical_measure(f, μ::AbstractMeasure, f_combine)
+@inline function mbind(f, μ::AbstractMeasure, f_combine)
     F, M, G = Core.Typeof(f), Core.Typeof(m), Core.Typeof(f_combine)
     HierarchicalProductMeasure{F,M,G}(f, μ, f_combine)
 end
 
 
+"""
+    MeasureBase.split_combined(f_c, α::AbstractMeasure, ab)
 
-function _split_variate(::typeof(=>), ::AbstractMeasure, x::Pair)
+Splits a combined value `ab` that originated from combining a point `a_orig`
+from the space of a measure `α` with a point `b_orig` from the space of
+another measure `β` via `ab = f_c(a_orig, b_orig)`.
+
+So with `a_orig = rand(α)`, `b_orig = rand(β)` and
+`ab = f_c(a_orig, b_orig)`, the following must hold true:
+
+```julia
+a, b2 = split_combined(f_c, α, ab)
+a ≈ a_orig && b ≈ b_orig
+```
+"""
+function split_combined end
+
+function split_combined(::typeof(=>), ::AbstractMeasure, x::Pair)
     return x.first, x.second
 end
 
-function _split_variate(f_combine::F, μ_primary::AbstractMeasure, x) where F
+function split_combined(f_combine::F, μ_primary::AbstractMeasure, x) where F
     _split_variate_byvalue(f_combine, testvalue(μ), x)
 end
 
 # Necessary/helpful for type stability?
-function _split_variate(::Type{F}, μ::AbstractMeasure, x) where F
+function split_combined(::Type{F}, μ::AbstractMeasure, x) where F
     _split_variate_byvalue(F, testvalue(μ), x)
 end
 
@@ -130,30 +154,30 @@ end
 function localmeasure(μ::HierarchicalProductMeasure, x)
     h_local, x_rest = _localmeasure_with_rest(μ, x)
     if !isempty(x_rest)
-        throw(ArgumentError("Variate too long while computing localmeasure of HierarchicalMeasure"))
+        throw(ArgumentError("Variate too long while computing localmeasure of Bind"))
     end
     return h_local
 end
 
 
-@inline insupport(::HierarchicalMeasure, x) = NoFastInsupport()
+@inline insupport(::Bind, x) = NoFastInsupport()
 
-@inline getdof(μ::HierarchicalMeasure) = NoDOF{typeof(μ)}()
+@inline getdof(μ::Bind) = NoDOF{typeof(μ)}()
 
 # Bypass `checked_arg`, would require potentially costly evaluation of h.f:
-@inline checked_arg(::HierarchicalMeasure, x) = x
+@inline checked_arg(::Bind, x) = x
 
-rootmeasure(::HierarchicalMeasure) = throw(ArgumentError("root measure is implicit, but can't be instantiated, for HierarchicalMeasure"))
+rootmeasure(::Bind) = throw(ArgumentError("root measure is implicit, but can't be instantiated, for Bind"))
 
-basemeasure(::HierarchicalMeasure) = throw(ArgumentError("basemeasure is not available for HierarchicalMeasure"))
+basemeasure(::Bind) = throw(ArgumentError("basemeasure is not available for Bind"))
 
-logdensity_def(::HierarchicalMeasure, x) = throw(ArgumentError("logdensity_def is not available for HierarchicalMeasure"))
+logdensity_def(::Bind, x) = throw(ArgumentError("logdensity_def is not available for Bind"))
 
 
 # # TODO: Default implementation of unsafe_logdensityof is a bit inefficient
 # # for AutoFlatten, since variate will be split in `localmeasure` and then
 # # split again in log-density evaluation. Maybe add something like
-# function unsafe_logdensityof(h::HierarchicalMeasure, x)
+# function unsafe_logdensityof(h::Bind, x)
 #     local_primary, local_secondary, x_primary, x_secondary = ...
 #     # Need to call full logdensityof for h_secondary since x_secondary hasn't
 #     # been checked yet:
@@ -161,15 +185,14 @@ logdensity_def(::HierarchicalMeasure, x) = throw(ArgumentError("logdensity_def i
 # end
 
 
-function Base.rand(rng::Random.AbstractRNG, ::Type{T}, h::HierarchicalMeasure) where {T<:Real}
+function Base.rand(rng::Random.AbstractRNG, ::Type{T}, h::Bind) where {T<:Real}
     x_primary = rand(rng, T, h.m)
     x_secondary = rand(rng, T, h.f(x_primary))
     return _combine_variates(h.flatten_mode, x_primary, x_secondary)
 end
 
 
-
-function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::HierarchicalMeasure, x)
+function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::Bind, x)
     μ_primary = μ.m
     y_primary, x_secondary = _to_std_with_rest(flatten_mode, ν_inner, μ_primary, x)
     μ_secondary = μ.f(x_secondary)
@@ -179,22 +202,22 @@ end
 
 function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::AbstractMeasure, x)
     dof_μ = getdof(μ)
-    x_μ, x_rest = _split_variate(flatten_mode, μ, x)
+    x_μ, x_rest = split_combined(flatten_mode, μ, x)
     y = transport_to(ν_inner^dof_μ, μ, x_μ)
     return y, x_rest
 end
 
-function transport_def(ν::_PowerStdMeasure{1}, μ::HierarchicalMeasure, x)
+function transport_def(ν::_PowerStdMeasure{1}, μ::Bind, x)
     ν_inner = _get_inner_stdmeasure(ν)
     y, x_rest = _to_std_with_rest(ν_inner, μ, x)
     if !isempty(x_rest)
-        throw(ArgumentError("Variate too long during transport involving HierarchicalMeasure"))
+        throw(ArgumentError("Variate too long during transport involving Bind"))
     end
     return y
 end
 
 
-function _from_std_with_rest(ν::HierarchicalMeasure, μ_inner::StdMeasure, x)
+function _from_std_with_rest(ν::Bind, μ_inner::StdMeasure, x)
     ν_primary = ν.m
     y_primary, x_secondary = _from_std_with_rest(ν_primary, μ_inner, x)
     ν_secondary = ν.f(y_primary)
@@ -206,11 +229,11 @@ function _from_std_with_rest(ν::AbstractMeasure, μ_inner::StdMeasure, x)
     dof_ν = getdof(ν)
     len_x = length(eachindex(x))
 
-    # Since we can't check DOF of original HierarchicalMeasure, we could "run out x" if
+    # Since we can't check DOF of original Bind, we could "run out x" if
     # the original x was too short. `transport_to` below will detect this, but better
     # throw a more informative exception here:
     if len_x < dof_ν
-        throw(ArgumentError("Variate too short during transport involving HierarchicalMeasure"))
+        throw(ArgumentError("Variate too short during transport involving Bind"))
     end
 
     y = transport_to(ν, μ_inner^dof_ν, x[begin:begin+dof_ν-1])
@@ -218,13 +241,13 @@ function _from_std_with_rest(ν::AbstractMeasure, μ_inner::StdMeasure, x)
     return y, x_rest
 end
 
-function transport_def(ν::HierarchicalMeasure, μ::_PowerStdMeasure{1}, x)
+function transport_def(ν::Bind, μ::_PowerStdMeasure{1}, x)
     # Sanity check, should be checked by transport machinery already:
     @assert getdof(μ) == length(eachindex(x)) && x isa AbstractVector
     μ_inner = _get_inner_stdmeasure(μ)
     y, x_rest = _from_std_with_rest(ν, μ_inner, x)
     if !isempty(x_rest)
-        throw(ArgumentError("Variate too long during transport involving HierarchicalMeasure"))
+        throw(ArgumentError("Variate too long during transport involving Bind"))
     end
     return y
 end
