bc97abe91a643b6783f979de69ca897f704eee5a
Oliver Schulz <oschulz@mpp.mpg.de>
2023-06-29 21:25:01 +0200

    STASH hierarchical to bind

--- src/combinators/bind.jl
+++ src/combinators/bind.jl
@@ -1,45 +1,286 @@
-"""
-    struct MeasureBase.Bind{M,K} <: AbstractMeasure
+export Bind
 
-Represents a monatic bind. User code should create instances of `Bind`
-directly, but should call `mbind(k, μ)` instead.
 """
-struct Bind{M,K} <: AbstractMeasure
-    k::K
-    μ::M
+    struct MeasureBase.Bind{F,M<:AbstractMeasure,G} <: AbstractMeasure
+
+Represents a monatic bind resp. a mbind in general.
+
+User code should not create instances of `Bind` directly, but should call
+[`mbind`](@ref) instead.
+"""
+struct Bind{K,M<:AbstractMeasure,C} <: AbstractMeasure
+    f_kernel::K
+    m_primary::M
+    f_combine::C
 end
 
-getdof(d::Bind) = NoDOF{typeof(d)}()
 
-function Base.rand(rng::AbstractRNG, ::Type{T}, d::Bind) where {T}
-    x = rand(rng, T, d.μ)
-    y = rand(rng, T, d.k(x))
+@doc raw"""
+    mbind(f_β, α::AbstractMeasure, f_c = second)
+
+Constructs a monadic bind, resp. a hierarchical measure, from a transition
+kernel function `f_β`, a primary measure `α` and a variate combination
+function `f_c`.
+
+`f_β` must be a function that maps a point `a` from the space of the primary
+measure `α` to a dependent secondary measure `β_a = f_β(a)`.
+`ab = f_combine(a, b)` must map such a point `a` and a point `b` from the
+space of measure `β_a` to a combined value `ab = f_c(a, b)`.
+
+The resulting measure
+
+```julia
+μ = mbind(f_c, α, f_β)
+```
+
+has the mathethematical interpretation
+
+```math
+\mu(f_c(A, B)) = \int_A \beta_a(B)\, \mathrm{d}\, \alpha(a) 
+```
+
+When using the default `fc = second` (so `ab == b`) this simplies to
+
+```math
+\mu(B) = \int_A \beta_a(B)\, \mathrm{d}\, \alpha(a) 
+```
+
+which is equivalent to a monatic bind, viewing measures as monads.
+
+Computationally, `ab = rand(μ)` is equivalent to
+
+```julia
+a = rand(μ_primary)
+β_a = f_β(a)
+b = rand(β_a)
+ab = f_combine(a, b)
+```
+
+Densities on hierarchical measures can only be evaluated if `ab = f_c(a, b)`
+can be unambiguously split into `a` and `b` again, knowing `α`. This is
+currently implemented for `f_c` that is either `=>`/`Pair` or `tuple` (these
+work for any combination of variate types), `vcat` (for tuple- or vector-like
+variates) and `merge` (`NamedTuple` variates).
+[`MeasureBase.split_point(::typeof(f_c), α)`](@ref) can be specialized to
+support other choices for `f_c`.
+
+# Extended help
+
+Bayesian example with a correlated prior, that models the 
+
+```julia
+using MeasureBase
+
+prior = mbind
+    productmeasure((
+        value => StdNormal()
+    )), merge
+) do a
+    productmeasure((
+        noise = pushfwd(sqrt ∘ Mul(abs(a.position)), StdExponential())
+    ))
+end
+
+model = θ -> pushfwd(MulAdd(θ.noise, θ.value), StdNormal())^10
+
+joint_θ_obs = mbind(model, prior, tuple)
+prior_predictive = mbind(model, prior)
+
+observation = rand(prior_predictive)
+likelihood = likelihoodof(model, observation)
+
+posterior = mintegrate(likelihood, prior)
+
+θ = rand(prior)
+logdensityof(posterior, θ)
+```
+"""
+function mbind end
+export mbind
+
+@inline function mbind(f, μ::AbstractMeasure, f_combine = second)
+    F, M, G = Core.Typeof(f), Core.Typeof(m), Core.Typeof(f_combine)
+    HierarchicalProductMeasure{F,M,G}(f, μ, f_combine)
+end
+
+
+"""
+    MeasureBase.split_combined(f_c, α::AbstractMeasure, ab)
+
+Splits a combined value `ab` that originated from combining a point `a_orig`
+from the space of a measure `α` with a point `b_orig` from the space of
+another measure `β` via `ab = f_c(a_orig, b_orig)`.
+
+So with `a_orig = rand(α)`, `b_orig = rand(β)` and
+`ab = f_c(a_orig, b_orig)`, the following must hold true:
+
+```julia
+a, b2 = split_combined(f_c, α, ab)
+a ≈ a_orig && b ≈ b_orig
+```
+"""
+function split_combined end
+
+function split_combined(::typeof(=>), ::AbstractMeasure, x::Pair)
+    return x.first, x.second
+end
+
+function split_combined(f_combine::F, μ_primary::AbstractMeasure, x) where F
+    _split_variate_byvalue(f_combine, testvalue(μ), x)
+end
+
+# Necessary/helpful for type stability?
+function split_combined(::Type{F}, μ::AbstractMeasure, x) where F
+    _split_variate_byvalue(F, testvalue(μ), x)
+end
+
+function _split_variate_byvalue(test_primary::AbstractVector, x::AbstractVector)
+    n, m = length(eachindex(test_primary)), length(eachindex(x))
+    # TODO: Use getindex or view?
+    return x[begin:begin+n-1], x[begin+n:end]
+end
+
+function _split_variate_byvalue(::Tuple{N}, x::Tuple{M}) where {N,M}
+    return ntuple(i -> x[i], Val(1:N)), ntuple(i -> x[i], Val(N+1:M))
+end
+
+@generated function _split_variate_byvalue(::NamedTuple{names_a}, x::NamedTuple{names}) where {names_a,names}
+    # TODO: implement
+    @assert false
+end
+
+
+
+_combine_variates(::NoFlatten, a::Any, b::Any) = (a, b)
+
+
+_combine_variates(::AutoFlatten, a::Any, b::Any) = _autoflat_combine_variates(a, b)
+
+_autoflat_combine_variates(a::Any, b::Any) = (a, b)
+
+_autoflat_combine_variates(a::AbstractVector, b::AbstractVector) = vcat(a, b)
+
+_autoflat_combine_variates(a::Tuple, b::Tuple) = (a, b)
+
+# TODO: Check that names don't overlap:
+_autoflat_combine_variates(a::NamedTuple, b::NamedTuple) = merge(a, b)
+
+
+_local_productmeasure(::NoFlatten, μ1, μ2) = productmeasure(μ1, μ2)
+
+# TODO: _local_productmeasure(::AutoFlatten, μ1, μ2) = productmeasure(μ1, μ2)
+# Needs a FlatProductMeasure type.
+
+function _localmeasure_with_rest(μ::HierarchicalProductMeasure, x)
+    μ_primary = μ.m
+    local_primary, x_secondary = _localmeasure_with_rest(μ_primary, x)
+    μ_secondary = μ.f(x_secondary)
+    local_secondary, x_rest = _localmeasure_with_rest(μ_secondary, x_secondary)
+    return _local_productmeasure(μ.flatten_mode, local_primary, local_secondary), x_rest
+end
+
+function _localmeasure_with_rest(μ::AbstractMeasure, x)
+    x_checked = checked_arg(μ, x)
+    return localmeasure(μ, x_checked), Fill(zero(eltype(x)), 0)
+end
+
+function localmeasure(μ::HierarchicalProductMeasure, x)
+    h_local, x_rest = _localmeasure_with_rest(μ, x)
+    if !isempty(x_rest)
+        throw(ArgumentError("Variate too long while computing localmeasure of Bind"))
+    end
+    return h_local
+end
+
+
+@inline insupport(::Bind, x) = NoFastInsupport()
+
+@inline getdof(μ::Bind) = NoDOF{typeof(μ)}()
+
+# Bypass `checked_arg`, would require potentially costly evaluation of h.f:
+@inline checked_arg(::Bind, x) = x
+
+rootmeasure(::Bind) = throw(ArgumentError("root measure is implicit, but can't be instantiated, for Bind"))
+
+basemeasure(::Bind) = throw(ArgumentError("basemeasure is not available for Bind"))
+
+logdensity_def(::Bind, x) = throw(ArgumentError("logdensity_def is not available for Bind"))
+
+
+# # TODO: Default implementation of unsafe_logdensityof is a bit inefficient
+# # for AutoFlatten, since variate will be split in `localmeasure` and then
+# # split again in log-density evaluation. Maybe add something like
+# function unsafe_logdensityof(h::Bind, x)
+#     local_primary, local_secondary, x_primary, x_secondary = ...
+#     # Need to call full logdensityof for h_secondary since x_secondary hasn't
+#     # been checked yet:
+#     unsafe_logdensityof(local_primary, x_primary) + logdensityof(local_secondary, x_secondary)
+# end
+
+
+function Base.rand(rng::Random.AbstractRNG, ::Type{T}, h::Bind) where {T<:Real}
+    x_primary = rand(rng, T, h.m)
+    x_secondary = rand(rng, T, h.f(x_primary))
+    return _combine_variates(h.flatten_mode, x_primary, x_secondary)
+end
+
+
+function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::Bind, x)
+    μ_primary = μ.m
+    y_primary, x_secondary = _to_std_with_rest(flatten_mode, ν_inner, μ_primary, x)
+    μ_secondary = μ.f(x_secondary)
+    y_secondary, x_rest = _to_std_with_rest(flatten_mode, ν_inner, μ_secondary, x_secondary)
+    return _combine_variates(μ.flatten_mode, y_primary, y_secondary), x_rest
+end
+
+function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::AbstractMeasure, x)
+    dof_μ = getdof(μ)
+    x_μ, x_rest = split_combined(flatten_mode, μ, x)
+    y = transport_to(ν_inner^dof_μ, μ, x_μ)
+    return y, x_rest
+end
+
+function transport_def(ν::_PowerStdMeasure{1}, μ::Bind, x)
+    ν_inner = _get_inner_stdmeasure(ν)
+    y, x_rest = _to_std_with_rest(ν_inner, μ, x)
+    if !isempty(x_rest)
+        throw(ArgumentError("Variate too long during transport involving Bind"))
+    end
     return y
 end
 
-"""
-    mbind(k, μ)::AbstractMeasure
-    
-Given
 
-- a measure μ
-- a kernel function k that takes values from the support of μ and returns a
-  measure
-
-The *monadic bind* operation `mbind(k, μ)` returns is a new measure.
-If `ν == mbind(k, μ)` and all measures involved are sampleable, then 
-samples from `rand(ν)` follow the same distribution as those from `rand(k(rand(μ)))`.
-
-
-A monadic bind ofen written as `>>=` (e.g. in Haskell), but this symbol is
-unavailable in Julia.
-
-```
-μ = StdExponential()
-ν = mbind(μ) do scale
-    pushfwd(Base.Fix1(*, scale), StdNormal())
+function _from_std_with_rest(ν::Bind, μ_inner::StdMeasure, x)
+    ν_primary = ν.m
+    y_primary, x_secondary = _from_std_with_rest(ν_primary, μ_inner, x)
+    ν_secondary = ν.f(y_primary)
+    y_secondary, x_rest = _from_std_with_rest(ν_secondary, μ_inner, x_secondary)
+    return _combine_variates(ν.flatten_mode, y_primary, y_secondary), x_rest
 end
-```
-"""
-mbind(k, μ) = Bind(k, μ)
-export mbind
+
+function _from_std_with_rest(ν::AbstractMeasure, μ_inner::StdMeasure, x)
+    dof_ν = getdof(ν)
+    len_x = length(eachindex(x))
+
+    # Since we can't check DOF of original Bind, we could "run out x" if
+    # the original x was too short. `transport_to` below will detect this, but better
+    # throw a more informative exception here:
+    if len_x < dof_ν
+        throw(ArgumentError("Variate too short during transport involving Bind"))
+    end
+
+    y = transport_to(ν, μ_inner^dof_ν, x[begin:begin+dof_ν-1])
+    x_rest = Fill(zero(eltype(x)), dof_ν - len_x)
+    return y, x_rest
+end
+
+function transport_def(ν::Bind, μ::_PowerStdMeasure{1}, x)
+    # Sanity check, should be checked by transport machinery already:
+    @assert getdof(μ) == length(eachindex(x)) && x isa AbstractVector
+    μ_inner = _get_inner_stdmeasure(μ)
+    y, x_rest = _from_std_with_rest(ν, μ_inner, x)
+    if !isempty(x_rest)
+        throw(ArgumentError("Variate too long during transport involving Bind"))
+    end
+    return y
+end
---
+++ test/combinators/bind.jl
@@ -0,0 +1,11 @@
+using Test
+
+using MeasureBase
+using MeasureBase: AbstractMeasure
+using MeasureBase: StdExponential, StdLogistic, StdUniform
+using MeasureBase: pushfwd, pullbck, mbind, productmeasure
+using MeasureBase: mbind, mintegrate, mintegrate_exp, density_rel, logdensity_rel
+
+@testset "bind.jl" begin
+
+end
