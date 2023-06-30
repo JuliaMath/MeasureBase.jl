4b5b7a0dc4d5e446f3a7f181f956b7c1d300c27d
Oliver Schulz <oschulz@mpp.mpg.de>
2023-06-30 05:26:04 +0200

    STASH

--- src/combinators/bind.jl
+++ src/combinators/bind.jl
@@ -102,23 +102,60 @@ struct Bind{FK,M<:AbstractMeasure,FC} <: AbstractMeasure
 end
 
 
-_local_productmeasure(fc, α, \) = productmeasure(fc(marginals(μ1), marginals(μ2)))
+"""
+    MeasureBase.transportmeasure(μ::Bind, x)::AbstractMeasure
 
-# TODO: _local_productmeasure(::AutoFlatten, μ1, μ2) = productmeasure(μ1, μ2)
-# Needs a FlatProductMeasure type.
+Evaluates a monatic bind `μ` at a point `x`.
 
-function _local_split_combined(f_c, μ::Bind, x)
-    local_α, a, b_withrest = _local_split_combined(μ.α, x)
-    β_a = μ.f_β(a)
-    local_β_a, b, rest = _localmeasure_with_rest(β_a, b_withrest)
-    local_μ = productmeasure(μ.f_c(marginals(local_α), marginals(local_β_a)))
-    local_ab, rest = split_combined(μ.f_c, local_μ, ab)
-    return local_μ, local_ab, rest
+The resulting measure behaves like `μ` in the infinitesimal neighborhood
+of `x` in respect to density calculation and transport as well.
+"""
+function transportmeasure(μ::Bind, x)
+    tpm_α, a, b = tpmeasure_split_combined(μ.α, x)
+    tpm_β_a = transportmeasure(μ.f_β(a), b)
+    mcombine(μ.f_c, tpm_α, tpm_β_a)
 end
 
-function _local_split_combined(f_c, μ::AbstractMeasure, x_withrest)
-    x, rest = split_combined(f_c, μ, x_withrest)
-    return localmeasure(μ, x), x, rest
+localmeasure(μ::Bind, x) = transportmeasure(μ, x)
+
+
+tpmeasure_split_combined(f_c, μ::Bind, xy) = _bind_lsc(f_c, μ::Bind, xy)
+
+function _bind_lsc(f_c::typeof(tuple), μ::Bind, xy::Tuple{Vararg{Any,2}})
+    x, y = x[1], y[1]
+    tpm_μ = transportmeasure(μ, x)
+    return tpm_μ, x, y
+end
+
+function _bind_lsc(f_c::Type{Pair}, μ::Bind, xy::Pair)
+    x, y = x.first, y.second
+    tpm_μ = transportmeasure(μ, x)
+    return tpm_μ, x, y
+end
+
+const _CatBind{FC} = _BindBy{<:Any,<:Any,FC}
+
+_bind_lsc(f_c::typeof(vcat), μ::_CatBind{typeof{vcat}}, xy::AbstractVector) = _bind_lsc_cat(f_c, μ, xy)
+_bind_lsc(f_c::typeof(merge), μ::_CatBind{typeof{merge}}, xy::NamedTuple) = _bind_lsc_cat(f_c, μ, xy)
+
+function _bind_lsc_cat_lμabyxy(f_c, μ, xy)
+    tpm_α, a, by = tpmeasure_split_combined(μ.f_c, μ.α, xy)
+    β_a = μ.f_β(a)
+    tpm_β_a, b, y = tpmeasure_split_combined(f_c, β_a, by)
+    tpm_μ = mcombine(μ.f_c, tpm_α, tpm_β_a)
+    return tpm_μ, a, b, y, xy
+end
+
+function _bind_lsc_cat(f_c::typeof(vcat), μ::_CatBind{typeof{vcat}}, xy::AbstractVector)
+    tpm_μ, a, b, y, xy = _bind_lsc_cat_lμabyxy(f_c, μ, xy)
+    # Don't use `x = f_c(a, b)` here, would allocate, splitting xy can use views:
+    x, y = _split_after(xy, length(a) + length(b))
+    return tpm_μ, x, y
+end
+
+function _bind_lsc_cat(f_c::typeof(merge), μ::_CatBind{typeof{merge}}, xy::NamedTuple)
+    tpm_μ, a, b, y, xy = _bind_lsc_cat_lμabyxy(f_c, μ, xy)
+    return tpm_μ, f_c(a, b), y
 end
 
 
@@ -133,28 +170,39 @@ rootmeasure(::Bind) = throw(ArgumentError("root measure is implicit, but can't b
 
 basemeasure(::Bind) = throw(ArgumentError("basemeasure is not available for Bind"))
 
+testvalue(::Bind) = throw(ArgumentError("testvalue is not available for Bind"))
+
 logdensity_def(::Bind, x) = throw(ArgumentError("logdensity_def is not available for Bind"))
 
+# Specialize logdensityof to avoid duplicate calculations:
+function logdensityof(μ::Bind, x)
+    tpm_α, a, b = tpmeasure_split_combined(μ.α, x)
+    β_a = μ.f_β(a)
+    logdensityof(tpm_α, a) + logdensityof(β_a, b)
+end
 
-# # TODO: Default implementation of unsafe_logdensityof is a bit inefficient
-# # for AutoFlatten, since variate will be split in `localmeasure` and then
-# # split again in log-density evaluation. Maybe add something like
-# function unsafe_logdensityof(h::Bind, x)
-#     local_primary, local_secondary, x_primary, x_secondary = ...
-#     # Need to call full logdensityof for h_secondary since x_secondary hasn't
-#     # been checked yet:
-#     unsafe_logdensityof(local_primary, x_primary) + logdensityof(local_secondary, x_secondary)
-# end
-
-
-function Base.rand(rng::Random.AbstractRNG, ::Type{T}, h::Bind) where {T<:Real}
-    x_primary = rand(rng, T, h.m)
-    x_secondary = rand(rng, T, h.f(x_primary))
-    return _combine_variates(h.flatten_mode, x_primary, x_secondary)
+# Specialize unsafe_logdensityof to avoid duplicate calculations:
+function unsafe_logdensityof(μ::Bind, x)
+    tpm_α, a, b = tpmeasure_split_combined(μ.α, x)
+    β_a = μ.f_β(a)
+    unsafe_logdensityof(tpm_α, a) + unsafe_logdensityof(β_a, b)
 end
 
 
+function Base.rand(rng::Random.AbstractRNG, ::Type{T}, μ::Bind) where {T<:Real}
+    a = rand(rng, T, μ.α)
+    b = rand(rng, T, μ.f_β(a))
+    return μ.f_c(a, b)
+end
+
+
+
+
+#!!!!!!!!!!!!!!!!!! TODO:
+
+
 function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::Bind, x)
+    
     μ_primary = μ.m
     y_primary, x_secondary = _to_std_with_rest(flatten_mode, ν_inner, μ_primary, x)
     μ_secondary = μ.f(x_secondary)
