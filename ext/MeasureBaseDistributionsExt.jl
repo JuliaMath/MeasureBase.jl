5c3139ab74c2fe48079eea3178e57eee719d7ecf
Oliver Schulz <oschulz@mpp.mpg.de>
2024-09-15 16:16:44 +0200

    Add Distributions and DistributionsForwardDiff extensions

---
+++ ext/MeasureBaseDistributionsExt.jl
@@ -0,0 +1,8 @@
+# This file is a part of MeasureBase.jl, licensed under the MIT License (MIT).
+
+module MeasureBaseDistributionsExt
+
+using MeasureBase
+import Distributions
+
+end # module MeasureBaseDistributionsExt
