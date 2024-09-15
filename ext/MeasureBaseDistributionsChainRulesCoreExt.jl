aad3dae9ef8b4da8588259d421e0bbb85a7caf75
Oliver Schulz <oschulz@mpp.mpg.de>
2024-09-15 16:21:51 +0200

    Add DistributionsChainRulesCore extension

---
+++ ext/MeasureBaseDistributionsChainRulesCoreExt.jl
@@ -0,0 +1,9 @@
+# This file is a part of MeasureBase.jl, licensed under the MIT License (MIT).
+
+module MeasureBaseDistributionsChainRulesCoreExt
+
+using MeasureBase
+import Distributions
+import ChainRulesCore
+
+end # module MeasureBaseDistributionsChainRulesCoreExt
