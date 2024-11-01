f0e545c5632d3ec908ec9a06905cd53295d0e8f8
Oliver Schulz <oschulz@mpp.mpg.de>
2024-11-01 20:29:28 +0100

    STASH

--- src/combinators/bind.jl
+++ src/combinators/bind.jl
@@ -1,20 +1,3 @@
-export Bind
-
-"""
-    struct MeasureBase.Bind{F,M<:AbstractMeasure,G} <: AbstractMeasure
-
-Represents a monatic bind resp. a mbind in general.
-
-User code should not create instances of `Bind` directly, but should call
-[`mbind`](@ref) instead.
-"""
-struct Bind{K,M<:AbstractMeasure,C} <: AbstractMeasure
-    f_kernel::K
-    m_primary::M
-    f_combine::C
-end
-
-
 @doc raw"""
     mbind(f_β, α::AbstractMeasure, f_c = second)
 
@@ -24,7 +7,7 @@ function `f_c`.
 
 `f_β` must be a function that maps a point `a` from the space of the primary
 measure `α` to a dependent secondary measure `β_a = f_β(a)`.
-`ab = f_combine(a, b)` must map such a point `a` and a point `b` from the
+`ab = f_c(a, b)` must map such a point `a` and a point `b` from the
 space of measure `β_a` to a combined value `ab = f_c(a, b)`.
 
 The resulting measure
@@ -33,7 +16,7 @@ The resulting measure
 μ = mbind(f_c, α, f_β)
 ```
 
-has the mathethematical interpretation
+has the mathethematical interpretation (on sets $$A$$ and $$B$$)
 
 ```math
 \mu(f_c(A, B)) = \int_A \beta_a(B)\, \mathrm{d}\, \alpha(a) 
@@ -53,12 +36,12 @@ Computationally, `ab = rand(μ)` is equivalent to
 a = rand(μ_primary)
 β_a = f_β(a)
 b = rand(β_a)
-ab = f_combine(a, b)
+ab = f_c(a, b)
 ```
 
 Densities on hierarchical measures can only be evaluated if `ab = f_c(a, b)`
 can be unambiguously split into `a` and `b` again, knowing `α`. This is
-currently implemented for `f_c` that is either `=>`/`Pair` or `tuple` (these
+currently implemented for `f_c` that is either tuple or `=>`/`Pair` (these
 work for any combination of variate types), `vcat` (for tuple- or vector-like
 variates) and `merge` (`NamedTuple` variates).
 [`MeasureBase.split_point(::typeof(f_c), α)`](@ref) can be specialized to
@@ -98,98 +81,81 @@ logdensityof(posterior, θ)
 function mbind end
 export mbind
 
-@inline function mbind(f, μ::AbstractMeasure, f_combine = second)
-    F, M, G = Core.Typeof(f), Core.Typeof(m), Core.Typeof(f_combine)
-    HierarchicalProductMeasure{F,M,G}(f, μ, f_combine)
+@inline function mbind(f_β, α::AbstractMeasure, f_c = second)
+    F, M, G = Core.Typeof(f_β), Core.Typeof(α), Core.Typeof(f_c)
+    Bind{F,M,G}(f_β, α, f_c)
 end
 
 
 """
-    MeasureBase.split_combined(f_c, α::AbstractMeasure, ab)
+    struct MeasureBase.Bind <: AbstractMeasure
 
-Splits a combined value `ab` that originated from combining a point `a_orig`
-from the space of a measure `α` with a point `b_orig` from the space of
-another measure `β` via `ab = f_c(a_orig, b_orig)`.
+Represents a monatic bind resp. a mbind in general.
 
-So with `a_orig = rand(α)`, `b_orig = rand(β)` and
-`ab = f_c(a_orig, b_orig)`, the following must hold true:
-
-```julia
-a, b2 = split_combined(f_c, α, ab)
-a ≈ a_orig && b ≈ b_orig
-```
+User code should not create instances of `Bind` directly, but should call
+[`mbind`](@ref) instead.
 """
-function split_combined end
-
-function split_combined(::typeof(=>), ::AbstractMeasure, x::Pair)
-    return x.first, x.second
-end
-
-function split_combined(f_combine::F, μ_primary::AbstractMeasure, x) where F
-    _split_variate_byvalue(f_combine, testvalue(μ), x)
-end
-
-# Necessary/helpful for type stability?
-function split_combined(::Type{F}, μ::AbstractMeasure, x) where F
-    _split_variate_byvalue(F, testvalue(μ), x)
-end
-
-function _split_variate_byvalue(test_primary::AbstractVector, x::AbstractVector)
-    n, m = length(eachindex(test_primary)), length(eachindex(x))
-    # TODO: Use getindex or view?
-    return x[begin:begin+n-1], x[begin+n:end]
-end
-
-function _split_variate_byvalue(::Tuple{N}, x::Tuple{M}) where {N,M}
-    return ntuple(i -> x[i], Val(1:N)), ntuple(i -> x[i], Val(N+1:M))
-end
-
-@generated function _split_variate_byvalue(::NamedTuple{names_a}, x::NamedTuple{names}) where {names_a,names}
-    # TODO: implement
-    @assert false
+struct Bind{FK,M<:AbstractMeasure,FC} <: AbstractMeasure
+    f_β::FK
+    α::M
+    f_c::FC
 end
 
 
+"""
+    MeasureBase.transportmeasure(μ::Bind, x)::AbstractMeasure
 
-_combine_variates(::NoFlatten, a::Any, b::Any) = (a, b)
+Evaluates a monatic bind `μ` at a point `x`.
 
-
-_combine_variates(::AutoFlatten, a::Any, b::Any) = _autoflat_combine_variates(a, b)
-
-_autoflat_combine_variates(a::Any, b::Any) = (a, b)
-
-_autoflat_combine_variates(a::AbstractVector, b::AbstractVector) = vcat(a, b)
-
-_autoflat_combine_variates(a::Tuple, b::Tuple) = (a, b)
-
-# TODO: Check that names don't overlap:
-_autoflat_combine_variates(a::NamedTuple, b::NamedTuple) = merge(a, b)
-
-
-_local_productmeasure(::NoFlatten, μ1, μ2) = productmeasure(μ1, μ2)
-
-# TODO: _local_productmeasure(::AutoFlatten, μ1, μ2) = productmeasure(μ1, μ2)
-# Needs a FlatProductMeasure type.
-
-function _localmeasure_with_rest(μ::HierarchicalProductMeasure, x)
-    μ_primary = μ.m
-    local_primary, x_secondary = _localmeasure_with_rest(μ_primary, x)
-    μ_secondary = μ.f(x_secondary)
-    local_secondary, x_rest = _localmeasure_with_rest(μ_secondary, x_secondary)
-    return _local_productmeasure(μ.flatten_mode, local_primary, local_secondary), x_rest
+The resulting measure behaves like `μ` in the infinitesimal neighborhood
+of `x` in respect to density calculation and transport as well.
+"""
+function transportmeasure(μ::Bind, x)
+    tpm_α, a, b = tpmeasure_split_combined(μ.α, x)
+    tpm_β_a = transportmeasure(μ.f_β(a), b)
+    mcombine(μ.f_c, tpm_α, tpm_β_a)
 end
 
-function _localmeasure_with_rest(μ::AbstractMeasure, x)
-    x_checked = checked_arg(μ, x)
-    return localmeasure(μ, x_checked), Fill(zero(eltype(x)), 0)
+localmeasure(μ::Bind, x) = transportmeasure(μ, x)
+
+
+tpmeasure_split_combined(f_c, μ::Bind, xy) = _bind_tsc(f_c, μ::Bind, xy)
+
+function _bind_tsc(f_c::typeof(tuple), μ::Bind, xy::Tuple{Vararg{Any,2}})
+    x, y = x[1], y[1]
+    tpm_μ = transportmeasure(μ, x)
+    return tpm_μ, x, y
 end
 
-function localmeasure(μ::HierarchicalProductMeasure, x)
-    h_local, x_rest = _localmeasure_with_rest(μ, x)
-    if !isempty(x_rest)
-        throw(ArgumentError("Variate too long while computing localmeasure of Bind"))
-    end
-    return h_local
+function _bind_tsc(f_c::Type{Pair}, μ::Bind, xy::Pair)
+    x, y = x.first, y.second
+    tpm_μ = transportmeasure(μ, x)
+    return tpm_μ, x, y
+end
+
+const _CatBind{FC} = _BindBy{<:Any,<:Any,FC}
+
+_bind_tsc(f_c::typeof(vcat), μ::_CatBind{typeof{vcat}}, xy::AbstractVector) = _bind_tsc_cat(f_c, μ, xy)
+_bind_tsc(f_c::typeof(merge), μ::_CatBind{typeof{merge}}, xy::NamedTuple) = _bind_tsc_cat(f_c, μ, xy)
+
+function _bind_tsc_cat_lμabyxy(f_c, μ, xy)
+    tpm_α, a, by = tpmeasure_split_combined(μ.f_c, μ.α, xy)
+    β_a = μ.f_β(a)
+    tpm_β_a, b, y = tpmeasure_split_combined(f_c, β_a, by)
+    tpm_μ = mcombine(μ.f_c, tpm_α, tpm_β_a)
+    return tpm_μ, a, b, y, xy
+end
+
+function _bind_tsc_cat(f_c::typeof(vcat), μ::_CatBind{typeof{vcat}}, xy::AbstractVector)
+    tpm_μ, a, b, y, xy = _bind_tsc_cat_lμabyxy(f_c, μ, xy)
+    # Don't use `x = f_c(a, b)` here, would allocate, splitting xy can use views:
+    x, y = _split_after(xy, length(a) + length(b))
+    return tpm_μ, x, y
+end
+
+function _bind_tsc_cat(f_c::typeof(merge), μ::_CatBind{typeof{merge}}, xy::NamedTuple)
+    tpm_μ, a, b, y, xy = _bind_tsc_cat_lμabyxy(f_c, μ, xy)
+    return tpm_μ, f_c(a, b), y
 end
 
 
@@ -204,62 +170,53 @@ rootmeasure(::Bind) = throw(ArgumentError("root measure is implicit, but can't b
 
 basemeasure(::Bind) = throw(ArgumentError("basemeasure is not available for Bind"))
 
+testvalue(::Bind) = throw(ArgumentError("testvalue is not available for Bind"))
+
 logdensity_def(::Bind, x) = throw(ArgumentError("logdensity_def is not available for Bind"))
 
+# Specialize logdensityof to avoid duplicate calculations:
+function logdensityof(μ::Bind, x)
+    tpm_α, a, b = tpmeasure_split_combined(μ.α, x)
+    β_a = μ.f_β(a)
+    logdensityof(tpm_α, a) + logdensityof(β_a, b)
+end
 
-# # TODO: Default implementation of unsafe_logdensityof is a bit inefficient
-# # for AutoFlatten, since variate will be split in `localmeasure` and then
-# # split again in log-density evaluation. Maybe add something like
-# function unsafe_logdensityof(h::Bind, x)
-#     local_primary, local_secondary, x_primary, x_secondary = ...
-#     # Need to call full logdensityof for h_secondary since x_secondary hasn't
-#     # been checked yet:
-#     unsafe_logdensityof(local_primary, x_primary) + logdensityof(local_secondary, x_secondary)
-# end
-
-
-function Base.rand(rng::Random.AbstractRNG, ::Type{T}, h::Bind) where {T<:Real}
-    x_primary = rand(rng, T, h.m)
-    x_secondary = rand(rng, T, h.f(x_primary))
-    return _combine_variates(h.flatten_mode, x_primary, x_secondary)
+# Specialize unsafe_logdensityof to avoid duplicate calculations:
+function unsafe_logdensityof(μ::Bind, x)
+    tpm_α, a, b = tpmeasure_split_combined(μ.α, x)
+    β_a = μ.f_β(a)
+    unsafe_logdensityof(tpm_α, a) + unsafe_logdensityof(β_a, b)
 end
 
 
-function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::Bind, x)
-    μ_primary = μ.m
-    y_primary, x_secondary = _to_std_with_rest(flatten_mode, ν_inner, μ_primary, x)
-    μ_secondary = μ.f(x_secondary)
-    y_secondary, x_rest = _to_std_with_rest(flatten_mode, ν_inner, μ_secondary, x_secondary)
-    return _combine_variates(μ.flatten_mode, y_primary, y_secondary), x_rest
+function Base.rand(rng::Random.AbstractRNG, ::Type{T}, μ::Bind) where {T<:Real}
+    a = rand(rng, T, μ.α)
+    b = rand(rng, T, μ.f_β(a))
+    return μ.f_c(a, b)
 end
 
-function _to_std_with_rest(flatten_mode::FlattenMode, ν_inner::StdMeasure, μ::AbstractMeasure, x)
-    dof_μ = getdof(μ)
-    x_μ, x_rest = split_combined(flatten_mode, μ, x)
-    y = transport_to(ν_inner^dof_μ, μ, x_μ)
-    return y, x_rest
-end
+
 
 function transport_def(ν::_PowerStdMeasure{1}, μ::Bind, x)
-    ν_inner = _get_inner_stdmeasure(ν)
-    y, x_rest = _to_std_with_rest(ν_inner, μ, x)
-    if !isempty(x_rest)
-        throw(ArgumentError("Variate too long during transport involving Bind"))
-    end
-    return y
+    tpm_μ = transportmeasure(μ, x)
+    return transport_def(ν, tpm_μ, x)
 end
 
 
 function _from_std_with_rest(ν::Bind, μ_inner::StdMeasure, x)
-    ν_primary = ν.m
-    y_primary, x_secondary = _from_std_with_rest(ν_primary, μ_inner, x)
-    ν_secondary = ν.f(y_primary)
-    y_secondary, x_rest = _from_std_with_rest(ν_secondary, μ_inner, x_secondary)
-    return _combine_variates(ν.flatten_mode, y_primary, y_secondary), x_rest
+    a, x2 = _from_std_with_rest(ν.α, μ_inner, x)
+    β_a = ν.f_β(a)
+    b, x_rest = _from_std_with_rest(β_a, μ_inner, x2)
+    return ν.f_c(a, b), x_rest
 end
 
 function _from_std_with_rest(ν::AbstractMeasure, μ_inner::StdMeasure, x)
     dof_ν = getdof(ν)
+    origin = transport_origin(ν)
+    return _from_std_with_rest_withdof(ν, getdof(ν), μ_inner, x, dof_ν, origin)
+end
+
+function _from_std_with_rest_withdof(ν::AbstractMeasure, dof_ν, μ_inner::StdMeasure, x)
     len_x = length(eachindex(x))
 
     # Since we can't check DOF of original Bind, we could "run out x" if
@@ -269,9 +226,22 @@ function _from_std_with_rest(ν::AbstractMeasure, μ_inner::StdMeasure, x)
         throw(ArgumentError("Variate too short during transport involving Bind"))
     end
 
-    y = transport_to(ν, μ_inner^dof_ν, x[begin:begin+dof_ν-1])
-    x_rest = Fill(zero(eltype(x)), dof_ν - len_x)
+    x_inner_dof, x_rest = _split_after(x, dof_ν)
+    y = transport_to(ν, μ_inner^dof_ν, x_inner_dof)
     return y, x_rest
+end
+
+function _from_std_with_rest_withdof(ν::AbstractMeasure, ::NoDOF, μ_inner::StdMeasure, x)
+    _from_std_with_rest_withorigin(ν, transport_origin(ν), μ_inner, x)
+end
+
+function _from_std_with_rest_withorigin(ν::AbstractMeasure, ν_origin, μ_inner::StdMeasure, x)
+    x_origin, x_rest = _from_std_with_rest(ν_origin, x, μ_inner)
+    from_origin(x_origin), x_rest
+end
+
+function _from_std_with_rest_withorigin(ν::AbstractMeasure, NoTransportOrigin, μ_inner::StdMeasure, x)
+    throw(ArgumentError("Don't know how to transport value of type $(nameof(typeof(x))) from power of $(nameof(typeof(μ_inner))) to $(nameof(typeof(ν)))"))
 end
 
 function transport_def(ν::Bind, μ::_PowerStdMeasure{1}, x)
