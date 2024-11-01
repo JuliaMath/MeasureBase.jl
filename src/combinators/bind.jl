f500aea9f7b6d508e8146d25998aa38eb2ba0391
Oliver Schulz <oschulz@mpp.mpg.de>
2024-11-01 20:28:08 +0100

    Rename bind to mbind and deprecate rightarrowtail
    Bind has too much naming conflict potential with Base.bind.
    
    The rightarrowtail operator looks very similar to the `>=>` "fish"
    operator (e.g. in Haskell), which is not a monadic bind.

--- src/combinators/bind.jl
+++ src/combinators/bind.jl
@@ -3,34 +3,47 @@ struct Bind{M,K} <: AbstractMeasure
     k::K
 end
 
-export ↣
 
 """
-If 
-- μ is an `AbstractMeasure` or satisfies the Measure interface, and
-- k is a function taking values from the support of μ and returning a measure
+    mbind(k, μ)::AbstractMeasure
+    
+Given
 
-Then `μ ↣ k` is a measure, called a *monadic bind*. In a
-probabilistic programming language like Soss.jl, this could be expressed as
+- a measure μ
+- a kernel function k that takes values from the support of μ and returns a
+  measure
 
-Note that bind is usually written `>>=`, but this symbol is unavailable in Julia.
+The *monadic bind* operation `mbind(k, μ)` returns is a new measure.
+
+A monadic bind ofen written as `>>=` (e.g. in Haskell), but this symbol is
+unavailable in Julia.
 
 ```
-bind = @model μ,k begin 
-    x ~ μ
-    y ~ k(x)
-    return y
+μ = StdExponential()
+ν = mbind(μ) do scale
+    pushfwd(Base.Fix1(*, scale), StdNormal())
 end
 ```
-
-See also `bind` and `Bind`
 """
-↣(μ, k) = bind(μ, k)
-
-bind(μ, k) = Bind(μ, k)
+mbind(k, μ) = Bind(μ, k)
+export mbind
 
 function Base.rand(rng::AbstractRNG, ::Type{T}, d::Bind) where {T}
     x = rand(rng, T, d.μ)
     y = rand(rng, T, d.k(x))
     return y
 end
+
+
+# ToDo: Remove `bind` (breaking).
+@noinline function bind(μ, k)
+    Base.depwarn("`foo(μ, k)` is deprecated, use `mbind(k, μ)` instead.", :bind)
+    mbind(k, μ)
+end
+
+
+# ToDo: Remove `↣` (breaking): It looks too similar to the `>=>` "fish"
+# operator (e.g. in Haskell) that is typically understood to take two monadic
+# functions as an argument, while a bind take a monad and a monadic functions.
+@deprecate ↣(μ, k) mbind(μ, k)
+export ↣
