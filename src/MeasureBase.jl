465152eb3119c7015997ddd56719d73d39c8d12d
Oliver Schulz <oschulz@mpp.mpg.de>
2024-11-09 17:12:04 -0500

    STASH mapped data

--- src/MeasureBase.jl
+++ src/MeasureBase.jl
@@ -144,7 +144,6 @@ include("combinators/restricted.jl")
 include("combinators/smart-constructors.jl")
 include("combinators/powerweighted.jl")
 include("combinators/conditional.jl")
-include("combinators/implicitlymapped.jl")
 include("combinators/likelihood.jl")
 
 include("standard/stdmeasure.jl")
@@ -158,6 +157,7 @@ include("combinators/half.jl")
 
 include("rand.jl")
 
+include("mapped_data.jl")
 include("density.jl")
 include("density-core.jl")
 
