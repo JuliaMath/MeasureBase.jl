2cedf7082304061a41aff891b4a5aca839130184
Oliver Schulz <oschulz@mpp.mpg.de>
2024-11-09 22:13:47 -0500

    STASH mapped data

--- src/mapped_data.jl
+++ src/mapped_data.jl
@@ -1,83 +1,37 @@
-
 """
-    abstract type MappedObservation
+    MeasureBase.is_mapped_obs(::Type{Obs})
 
-Supertype for observation/data that has been mapped, and so implies a
-pushforward on measures in the context of `logdensityof` and `likelihoodof`.
+Return `Static.True()` or `Static.False()` if observations of type `Obs` may
+be mapped observations (resp. a mapped points in a measurable space in
+general).
 
-The explicit map/function can only be determined given some kind of observed
-result `obs` using
+A return value of `Static.True()` indicates that `map_result_and_func(obs::Obs)`
+may return a different result than `(identity, obs)`. A return value of
+`Static.False()` gurantees that `map_result_and_func(obs::Obs)` will always
+return `(identity, obs)`.
 
-```julia
-f_map = explicit_mapfunc(mapped::MappedObservation, obs)
-```
-
-The original object that has been implicitly mapped
-may be retrieved via
-
-```julia
-obj = explicit_mapfunc(mapped::MappedObservation, obs)
-```
-
-Note that `obs` is typically *not* the directly result of `f_map(ob)`. Instead,
-the relationship between `obj`, `f_map`, and `obs` depends on what `obj` is:
-
-* A measure `mu = obj`: The mapping process is equivalent to
-  `mapped_mu = pushfwd(f_map, mu, PushfwdRootMeasure())` and `obs` is an
-  element of the measurable space of `mu`. Implicitly mapped measures support
-
-  ```julia
-  DensityInterface.DensityKind(mapped_mu::MappedObservation)
-  DensityInterface.logdensityof(mapped_mu::MappedObservation, obs)
-  ```
-
-  and the explicitly mapped measure can be generated via
-
-  ```julia
-  explicit_measure(mapped_mu::MappedObservation, obs)
-  ```
-
-* A transition/Markov kernel `f_kernel = obj`, i.e. a function that maps
-  points in some space to measures on a (possibly different) space:
-  The mapping process is equivalent to
-  `mapped_f_kernel = (p -> pushfwd(f_map, f_kernel(p), PushfwdRootMeasure()))`
-  and `obs` is an element of the measurable space of the measures generated
-  by the mapped kernel. Implicitly mapped transition/Markov kernels support
-
-  ```julia
-  Likelihood(mapped_f_kernel::MappedObservation, obs)
-  ```
-
-  and the explicitly mapped kernel can be generated via
-
-  ```julia
-  explicit_measure(mapped_mu::MappedObservation, obs)
-  ```
-
-# Implementation
-
-Subtypes of `MappedObservation` that should support origin measures of
-type `SomeRelevantMeasure` and observations of type `SomeRelevantObs`,
-resulting in explicit maps/functions of type `SomeMapFunc`, must
-implement/specialize
-
-```julia
-MeasureBase.implicit_origin(mapped::MyImplicitlyMapped)
-MeasureBase.explicit_mapfunc(mapped::MyImplicitlyMapped, obs::SomeRelevantObs)::SomeMapFunc
-```
-
-and (except if functions of type `SomeMapFunc` are invertible via
-`InverseFunctions.inverse`) must also specialize
-
-```julia
-MeasureBase.pushfwd(f::SomeMapFunc, mu::SomeRelevantMeasure, ::PushfwdRootMeasure)
-```
-
-Subtypes of `MappedObservation` may support multiple combinations of
-observation and measure types.
+See also [`map_result_and_func`](@ref).
 """
-abstract type MappedObservation end
-export MappedObservation
+function is_mapped_obs end
+
+# ToDo: Use generated function?
+@inline function is_mapped_obs(::Type{T}) where {T}
+    if T isa DataType
+        for ST in T.parameters
+            ST isa Type && is_mapped_obs(ST) isa True && return True()
+        end
+        return False()
+    elseif T isa Union
+        is_mapped_obs(T.a) isa True && return True()
+        is_mapped_obs(T.b) isa True && return True()
+        return False()
+    elseif T isa UnionAll
+        return is_mapped_obs(T.body)
+    else
+        # Shouldn't reach this point
+        return False()
+    end
+end
 
 """
     map_result_and_func(tagged_y)
@@ -98,7 +52,7 @@ For example, with
 mu = productmeasure((a = StdUniform(), b = StdNormal(), c = StdExponential()))
 
 ```julia
-f, y == map_result_and_func(was_marginalized((a = 0.7, c = 1.2)))
+f, y == map_result_and_func(WasMarginalized((a = 0.7, c = 1.2)))
 x = (a = 0.7, b = something, c = 1.2, d = something_else)
 ````
 
@@ -108,59 +62,66 @@ will result in
 y == (a = 0.7, c = 1.2)
 y == f(x)
 ```
+
+# Implementation
+
+To when specializing `map_result_and_func` for additonal data types,
+[`MeasureBase.is_mapped_obs`] must be specialized as well to return
+`Static.True()` for the type.
 """
 function map_result_and_func end
 export map_result_and_func
 
-function DensityInterface.logdensityof(f_kernel::Base.Callable, x)
-    f_map, x_unwrapped = map_result_and_func(x)
-    mapped_kernel = _map_kernel(f_map, f_kernel)
+#!!!!! ToDo: Imlement properly (needs a generated function)
+@inline map_result_and_func(tagged_y) = (identity, tagged_y)
 
-    return (mapped_kernel, x_unwrapped)
+"""
+    mapped_measure_and_obs(μ, maybe_tagged_y)
+
+If `maybe_tagged_y` implies a mapping function `f_map` and a mapped value `y`,
+construct the pushforward `ν` of `μ` under `f_map` and return `(f_map, y)`,
+otherwise returns `(μ, tagged_y)`.
+
+See [`map_result_and_func`](@ref) for details on mapped observations.
+"""
+function mapped_measure_and_obs(μ, tagged_y)
+    _mapped_measure_and_obs_impl(is_mapped_obs(tagged_y), μ, tagged_y)
+end
+export mapped_measure_and_obs
+
+function _mapped_measure_and_obs_impl(::True, μ, tagged_y)
+    f_map, y = map_result_and_func(tagged_y)
+    ν = pushfwd(f_map, μ)
+    return (ν, y)
 end
 
-function likelihoodof(f_kernel::Base.Callable, x)
-    f_map, x_unwrapped = map_result_and_func(x)
-    mapped_kernel = _map_kernel(f_map, f_kernel)
+_mapped_measure_and_obs_impl(::False, μ, tagged_y) = (μ, tagged_y)
 
-    return Likelihood{Base.Typeof(mapped_kernel),Base.Typeof(x_unwrapped)}(
-        mapped_kernel,
-        x_unwrapped,
-    )
+"""
+    mapped_kernel_and_obs(f_kernel, maybe_tagged_y)
+
+If `maybe_tagged_y` implies a mapping function `f_map` and a mapped value `y`,
+construct `mapped_f_kernel` by composing a a pushforward under `f` with
+`f_map` and returns `(mapped_f_kernel, y)`. Otherwise, return
+`(f_kernel, maybe_tagged_y)`.
+
+See [`map_result_and_func`](@ref) for details on mapped observations.
+"""
+@inline function mapped_kernel_and_obs(f_kernel, tagged_y)
+    _mapped_kernel_and_obs_impl(is_mapped_obs(tagged_y), f_kernel, tagged_y)
 end
+export mapped_kernel_and_obs
 
-#!!!!!!!! make an object for this instead of using an anonymous function:
-_map_kernel(f_map, f_kernel) = (p -> pushfwd(f_map, f_kernel(p), PushfwdRootMeasure()))
+function _mapped_kernel_and_obs_impl(::True, f_kernel, tagged_y)
+    f_map, y = map_result_and_func(tagged_y)
+    mapped_k = _map_kernel(f_map, f_kernel)
+
+    return (mapped_k, y)
+end
+_map_kernel(f_map, f_kernel) = pushfwd(f_map, PushfwdRootMeasure()) ∘ f_kernel
 _map_kernel(::typeof(identity), f_kernel) = f_kernel
 
-function DensityInterface.DensityKind(mapped::MappedObservation)
-    DensityKind(implicit_origin(mapped))
-end
-
-"""
-    explicit_kernel(mapped::MappedObservation, obs)
-
-Get an expliclity mapped transition/Markov kernel, based on an implicitly
-mapped kernel and an observation that provides context on which pushforward
-to add to the unmapped original kernel `implicit_origin(mapped)`.
-
-Used [`explicit_mapfunc`](@ref) to get the function to use in the pushforward.
-
-# Implementation
-
-`explicit_kernel` does not need to be specialized for subtypes of
-`MappedObservation`.
-"""
-function explicit_kernel(mapped_kernel::MappedObservation, obs)
-    f_map = explicit_mapfunc(mapped_kernel, obs)
-    f_kernel = implicit_origin(mapped_kernel)
-    return (p -> pushfwd(f_map, f_kernel(p), PushfwdRootMeasure()))
-end
-export explicit_kernel
-
-function Likelihood(mapped_kernel::MappedObservation, obs)
-    return Likelihood(explicit_kernel(mapped_kernel, obs), obs)
-end
+_mapped_kernel_and_obs_impl(::False, f_kernel, tagged_y) = (f_kernel, tagged_y)
 
 """
     struct MeasureBase.TakeAny{T} <: Function
@@ -195,44 +156,35 @@ function (f::TakeAny)(xs)
 end
 
 """
-    struct Marginalized{T} <: MappedObservation
+    struct WasMarginalized{T}
 
-Represents an implicitly marginalized measure or transition kernel.
+Annotates a data/observation object to indicate that is was the result of
+marginalization.
 
 Constructors:
 
-* `Marginalized(mu)`
-* `Marginalized(f_kernel)`
+* `WasMarginalized(x)`
 
-See [MappedObservation](@ref) for detailed semantics.
-
-Example:
-
-```julia
-mu = productmeasure((a = StdUniform(), b = StdNormal(), c = StdExponential()))
-obs = (a = 0.7, c = 1.2)
-
-marg_mu_equiv = productmeasure((a = StdUniform(), c = StdExponential()))
-
-logdensityof(Marginalized(mu), obs) ≈ logdensityof(marg_mu_equiv, obs)
-```
+See [`map_result_and_func`](@ref) for more details.
 """
-struct Marginalized{T} <: MappedObservation
+struct WasMarginalized{T} <: MappedObservation
     obj::T
 end
-export Marginalized
+export WasMarginalized
 
-implicit_origin(mapped::Marginalized) = mapped.obj
+implicit_origin(mapped::WasMarginalized) = mapped.obj
 
-function explicit_mapfunc(::Marginalized, obs::NamedTuple{names}) where {names}
+function explicit_mapfunc(::WasMarginalized, obs::NamedTuple{names}) where {names}
     PropSelFunction{names,names}()
 end
 function pushfwd(f::PropSelFunction, mu::ProductMeasure{<:NamedTuple}, ::PushfwdRootMeasure)
     productmeasure(f(marginals(mu)))
 end
 
-explicit_mapfunc(::Marginalized, obs::AbstractVector) = TakeAny(length(obs))
-explicit_mapfunc(::Marginalized, obs::StaticArray{Tuple{N}}) where {N} = TakeAny(static(N))
+explicit_mapfunc(::WasMarginalized, obs::AbstractVector) = TakeAny(length(obs))
+function explicit_mapfunc(::WasMarginalized, obs::StaticArray{Tuple{N}}) where {N}
+    TakeAny(static(N))
+end
 
 function pushfwd(
     f::TakeAny,
